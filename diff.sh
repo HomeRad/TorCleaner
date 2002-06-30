ea6d385cda0d76a1c96a4b467deded228a3ba875
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2002-06-30 13:24:35 +0000

    use user-agent
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@624 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- diff.sh
+++ diff.sh
@@ -4,6 +4,7 @@ PYTHON=/usr/bin/python2.1
 $PYTHON webcleaner restart
 sleep 4
 env http_proxy="" wget -t1 -O unfiltered.html $1
-env http_proxy="http://localhost:9090" wget -t1 -O filtered.html $1
+# use CalZilla as User-Agent, some sites are blocking spiders (eg. IMDB)
+env http_proxy="http://localhost:9090" wget -t1 -U CalZilla -O filtered.html $1
 diff -BuN unfiltered.html filtered.html
 rm -f unfiltered.html filtered.html
