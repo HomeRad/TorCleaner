cb6339145ccb6c7c2a797d696c96dfd7f109968d
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2002-08-21 21:20:53 +0000

    accept more chars in start- and endtags, added testcases to the testsuite
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@681 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/parsefile.py
+++ test/parsefile.py
@@ -4,12 +4,12 @@ sys.path.insert(0, ".")
 from wc.parser.htmllib import HtmlPrinter
 
 def _main():
-    pass
-    #file = sys.argv[1]
-    #data = open(file).read()
-    #p = HtmlPrinter()
-    #p.feed(data)
-    #p.flush()
+    #pass
+    file = sys.argv[1]
+    data = open(file).read()
+    p = HtmlPrinter()
+    p.feed(data)
+    p.flush()
 
 if __name__=='__main__':
     _main()
