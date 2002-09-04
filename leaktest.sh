ad12855bbfd7ade185f0d6a23e78db2ea508fda9
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2002-09-04 19:50:00 +0000

    leaktest for webcleaner
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@685 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- leaktest.sh
+++ leaktest.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # install valgrind to run this script
 #F=/home/calvin/documents/iptables-tutorial.html
-F=test/html/1.html
-valgrind --leak-check=yes python2.1 -d test/filterfile.py $F
+#F=test/html/1.html
+#valgrind --leak-check=yes python2.1 -d test/filterfile.py $F
+valgrind --leak-check=yes python webcleaner startfunc
