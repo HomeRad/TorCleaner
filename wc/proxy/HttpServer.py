395613d688680fd7dcc0bb39f574c90b18e1cd33
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2002-09-10 23:55:53 +0000

    identity encoding
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@706 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- wc/proxy/HttpServer.py
+++ wc/proxy/HttpServer.py
@@ -235,7 +235,7 @@ class HttpServer (Server):
                 self.decoders.append(GunzipStream())
             # remove encoding because we unzip the stream
             remove_headers(self.headers, ['Content-Encoding'])
-        elif encoding:
+        elif encoding and encoding!='identity':
             print >>sys.stderr, "Warning: unsupported encoding", `encoding`
         # initStateObject can modify headers (see Compress.py)!
         self.attrs = initStateObjects(self.headers, self.url)
