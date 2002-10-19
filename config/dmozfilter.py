5daea88bae44be281173f4182c38c20c54c5a270
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2002-10-19 11:18:50 +0000

    filter the big dmoz fucker
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@801 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ config/dmozfilter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# filter the big dmoz file
+# usage:
+# zcat content.rdf.u8.gz | dmozfilter.py | gzip --best > dmoz.rdf.stripped.gz
+import sys
+line = sys.stdin.readline()
+topic = None
+while line:
+    line = line.strip()
+    if line.startswith("<Topic r:id="):
+        _topic = line[13:line.rindex('"')]
+        if "/" in _topic:
+            topic = _topic.split("/", 2)[1].lower()
+    elif line.startswith("</Topic>"):
+        topic = None
+    if topic=='kids_and_teens':
+        print line
+    line = sys.stdin.readline()
