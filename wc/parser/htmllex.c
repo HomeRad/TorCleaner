5bae889ef0521ae4143c95ea617ceb63d791147b
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-01-20 06:46:07 +0000

    flex 2.5.26
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@948 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- wc/parser/htmllex.c
+++ wc/parser/htmllex.c
@@ -9,7 +9,7 @@
 #define FLEX_SCANNER
 #define YY_FLEX_MAJOR_VERSION 2
 #define YY_FLEX_MINOR_VERSION 5
-#define YY_FLEX_SUBMINOR_VERSION 25
+#define YY_FLEX_SUBMINOR_VERSION 26
 #if YY_FLEX_SUBMINOR_VERSION > 0
 #define FLEX_BETA
 #endif
@@ -43,19 +43,9 @@ typedef uint32_t flex_uint32_t;
 typedef signed char flex_int8_t;
 typedef short int flex_int16_t;
 typedef int flex_int32_t;
-# if __WORDSIZE == 64 || defined __arch64__ || defined __cplusplus
-typedef long int flex_int64_t;
-# else
-typedef long long int flex_int64_t;
-# endif
 typedef unsigned char flex_uint8_t; 
 typedef unsigned short int flex_uint16_t;
 typedef unsigned int flex_uint32_t;
-#if __WORDSIZE == 64 || defined __arch64__ || defined __cplusplus
-typedef unsigned long int flex_uint64_t;
-#else
-typedef unsigned long long int flex_uint64_t;
-#endif
 #endif /* ! C99 */
 
 /* Limits of integral types. */
@@ -6864,7 +6854,7 @@ static PyObject* quote_string (PyObject* val) {
 
 #define S_STRING 16
 
-#line 6868 "htmllex.c"
+#line 6858 "htmllex.c"
 #endif /* !yyIN_HEADER YY-END-DISCARD-FROM-HEADER */
 
 /* Special case for "unistd.h", since it is non-ANSI. We include it way
@@ -7182,7 +7172,7 @@ YY_DECL
 
 
   /*********************** EOF ************************/
-#line 7186 "htmllex.c"
+#line 7176 "htmllex.c"
 
     yylval = yylvalp;
 #ifdef YYLTYPE
@@ -7199,7 +7189,7 @@ YY_DECL
 
 #ifdef YY_USES_REJECT
         if ( ! YY_G(yy_state_buf) )
-            YY_G(yy_state_buf) = yyalloc(YY_BUF_SIZE + 2  YY_CALL_LAST_ARG);
+            YY_G(yy_state_buf) = (yy_state_type *)yyalloc(YY_BUF_SIZE + 2  YY_CALL_LAST_ARG);
 #endif
 
 		if ( ! YY_G(yy_start) )
@@ -8197,7 +8187,7 @@ YY_RULE_SETUP
 #line 762 "htmllex.l"
 ECHO;
 	YY_BREAK
-#line 8201 "htmllex.c"
+#line 8191 "htmllex.c"
 
 	case YY_END_OF_BUFFER:
 		{
@@ -8611,7 +8601,7 @@ static int yy_get_next_buffer YYFARGS0(void)
 					/* Reset buffer status. */
 					yyrestart( yyin YY_CALL_LAST_ARG);
 
-					/* fall through */
+					/*FALLTHROUGH*/
 
 				case EOB_ACT_END_OF_FILE:
 					{
