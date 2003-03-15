6d45bde7b7237dcbf4adb9ba3d2745e159c54324
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-03-15 10:54:28 +0000

    more documentation
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@1044 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- wc/proxy/Listener.py
+++ wc/proxy/Listener.py
@@ -3,13 +3,20 @@ from wc import i18n, config
 from wc.debug import *
 
 class Listener (asyncore.dispatcher):
+    """A listener accepts connections on a specified port. Each
+       accepted incoming connection gets delegated to an instance of the
+       handler class"""
     def __init__ (self, port, handler):
+        """create a socket on specified port and start listening to it"""
         asyncore.dispatcher.__init__(self)
         self.addr = (('', 'localhost')[config['local_sockets_only']], port)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        # disable NAGLE algorithm, which means sending pending data
+        # immediately, possibly wasting bandwidth
         self.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         self.set_reuse_addr()
         self.bind(self.addr)
+        # 5 is the maximum number of queued connections
         self.listen(5)
         self.handler = handler
 
@@ -17,12 +24,15 @@ class Listener (asyncore.dispatcher):
         return '<Listener:%s>' % self.addr[1]
 
     def log (self, msg):
+        """standard logging is disabled, we dont need it here"""
         pass
 
     def writable (self):
+        """The listener is never writable, it returns None"""
         return None
 
     def handle_accept (self):
+        """start the handler class with the new socket"""
         #debug(HURT_ME_PLENTY, 'accept', self)
         apply(self.handler, self.accept())
 
