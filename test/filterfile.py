df1c433662bb098e6ed9beb56d5ba27d87bcf716
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-03-18 23:00:12 +0000

    better js test
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@1072 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/filterfile.py
+++ test/filterfile.py
@@ -6,14 +6,27 @@ def _main():
     f = sys.argv[1]
     data = file(f).read()
     import wc, time
-    wc.DebugLevel = 3
+    wc.set_debuglevel(wc.NIGHTMARE)
     wc.config = wc.Configuration()
+    wc.set_debuglevel(wc.NIGHTMARE)
     wc.config['filters'] = ['Replacer', 'Rewriter', 'BinaryCharFilter']
     wc.config.init_filter_modules()
+    from wc.proxy import proxy_poll, run_timers
+    from wc.filter import FilterException
     attrs = wc.filter.initStateObjects(url=f)
-    filtered = wc.filter.applyfilter(wc.filter.FILTER_RESPONSE_MODIFY,
-           data, 'finish', attrs)
-    print filtered
+    i = 1
+    while 1:
+        try:
+            filtered = wc.filter.applyfilter(wc.filter.FILTER_RESPONSE_MODIFY,
+                   data, 'finish', attrs)
+            print filtered
+        except FilterException, msg:
+            print msg
+            data = ""
+            proxy_poll(timeout=max(0, run_timers()))
+        i+=1
+        if i==5:
+            break
 
 
 if __name__=='__main__':
