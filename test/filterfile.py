99d2a1c7a367d85bd5db0743074e99de9d565711
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-03-26 20:34:18 +0000

    call proxy_poll for JavaScript testing
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@1080 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/filterfile.py
+++ test/filterfile.py
@@ -2,31 +2,42 @@
 import sys, os
 sys.path.insert(0, os.getcwd())
 
+
 def _main():
     f = sys.argv[1]
     data = file(f).read()
     import wc, time
+    # set debug level
     wc.set_debuglevel(wc.NIGHTMARE)
     wc.config = wc.Configuration()
+    # debug level could be reset, so set it again
     wc.set_debuglevel(wc.NIGHTMARE)
     wc.config['filters'] = ['Replacer', 'Rewriter', 'BinaryCharFilter']
     wc.config.init_filter_modules()
     from wc.proxy import proxy_poll, run_timers
     from wc.filter import FilterException
     attrs = wc.filter.initStateObjects(url=f)
+    filtered = ""
+    for c in data:
+        try:
+            filtered += wc.filter.applyfilter(wc.filter.FILTER_RESPONSE_MODIFY,
+                                              c, 'filter', attrs)
+        except FilterException, msg:
+            print "Test: filter:", msg
     i = 1
     while 1:
         try:
             filtered = wc.filter.applyfilter(wc.filter.FILTER_RESPONSE_MODIFY,
-                   data, 'finish', attrs)
-            print filtered
+                                             "", 'finish', attrs)
+            break
         except FilterException, msg:
-            print msg
-            data = ""
+            print "Test: finish:", msg
             proxy_poll(timeout=max(0, run_timers()))
         i+=1
-        if i==5:
+        if i==6:
+            print "Test: oooooops"
             break
+    print filtered
 
 
 if __name__=='__main__':
