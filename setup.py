aa74b3dd5ad8b981766e15b2fe501567ce05f911
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-04-12 15:51:45 +0000

    updated
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@1191 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- setup.py
+++ setup.py
@@ -157,7 +157,7 @@ if USE_JS:
 myname = "Bastian Kleineidam"
 myemail = "calvin@users.sourceforge.net"
 setup (name = "webcleaner",
-       version = "1.8",
+       version = "1.9",
        description = "a filtering HTTP proxy",
        author = myname,
        author_email = myemail,
