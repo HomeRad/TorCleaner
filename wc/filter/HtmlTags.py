decfea3b2391bbb9adfa4eb69f4d48be97fdd928
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-05-24 09:43:53 +0000

    import re
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@1358 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- wc/filter/HtmlTags.py
+++ wc/filter/HtmlTags.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-import sys
+import sys, re
 if __name__=='__main__':
     import os
     sys.path.insert(0, os.getcwd())
