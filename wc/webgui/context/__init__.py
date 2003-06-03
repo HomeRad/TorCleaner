5b62968676c85d3a3ef6dde64458a5dcbe8e54b6
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-06-03 21:05:34 +0000

    added
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@1461 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ wc/webgui/context/__init__.py
@@ -0,0 +1,2 @@
+"""for each template/<theme>/<file>.html there is a context/<file>_html.py
+module delivering the page content values"""
---
+++ wc/webgui/context/error_html.py
@@ -0,0 +1,5 @@
+from wc import i18n, AppName
+
+title = i18n._("%s error") % AppName
+# this will most likely be overridden
+error = i18n._("%s error") % AppName
