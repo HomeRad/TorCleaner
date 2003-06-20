258e3d40020564e5caecfa22d1a348c64b6d8ab7
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-06-20 18:01:55 +0000

    work in a configuration copy
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@1561 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- wc/webgui/context/config_html.py
+++ wc/webgui/context/config_html.py
@@ -1,5 +1,5 @@
 from wc import i18n, AppName
-from wc import config as _config
+from wc import Configuration as _Configuration
 
 title = i18n._("%s configuration") % AppName
 port = i18n._("Port number")
@@ -7,17 +7,20 @@ configapply = i18n._("Apply")
 info = []
 error = []
 
+
 def exec_form (form):
     # reset info/error
     del info[:]
     del error[:]
+    # read current config (without filter stuff)
+    _config = _Configuration()
     if form.has_key('port'):
         try:
             _config['port'] = int(form['port'].value)
             info.append(i18n._("Port successfully changed"))
         except ValueError:
             error.append(i18n._("Invalid proxy port"))
-    # save changed config
     if info:
+        # write changed config
         _config.write_proxyconf()
 
