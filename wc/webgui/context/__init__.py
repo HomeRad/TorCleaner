d79a7dc8ff50362a5ae597af07625a15ec60142d
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-07-09 12:55:50 +0000

    oopsie
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@1659 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- wc/webgui/context/__init__.py
+++ wc/webgui/context/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: iso-8859-1 -*-
 """for each template/<theme>/<file>.html there is a context/<file>_html.py
 module delivering the page content values"""
+import re
 
 def getval (form, key):
     """return a formfield value"""
