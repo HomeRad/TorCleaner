939c1db03ff6cae4228508059393900e7c07bd6d
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-07-12 18:10:20 +0000

    accessibility, minor glitches
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@1713 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ wc/webgui/context/accessibility_html.py
@@ -0,0 +1,8 @@
+# -*- coding: iso-8859-1 -*-
+from wc import i18n, AppName
+
+title = i18n._("%s accessibility statement") % AppName
+t_skip_to_main = i18n._("Skip to main content")
+t_webcleaner = AppName
+t_proxyconf = i18n._("Proxy configuration")
+t_filterconf = i18n._("Filter configuration")
