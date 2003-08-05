9a206ca90450ed005204531f70706d379a0c3152
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-08-05 17:30:17 +0000

    clear locale-specific env vars before starting webcleaner
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@1835 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/single.sh
+++ test/single.sh
@@ -2,7 +2,7 @@
 # restart the webcleaner proxy, logging to local files
 # used for development test runs
 rm -f webcleaner.log webcleaner.err
-./webcleaner restart 2> webcleaner.err
+env -u LANGUAGE -u LC_ALL -u LC_CTYPE -u LANG ./webcleaner restart 2> webcleaner.err
 if [ "x$1" != "x" ]; then
     sleep 3
     env http_proxy="http://localhost:8080" wget "$1"
