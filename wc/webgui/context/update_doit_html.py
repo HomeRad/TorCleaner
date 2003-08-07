4750cf05419230d12a6685cd9650bb63f2332a53
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-08-07 06:29:56 +0000

    update cgi script
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@1859 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ wc/webgui/context/update_doit_html.py
@@ -0,0 +1,24 @@
+# -*- coding: iso-8859-1 -*-
+from wc import AppName, Version, BaseUrl
+from wc.update import update as _update
+from wc import Configuration as _Configuration
+from cStringIO import StringIO as _StringIO
+
+updatelog = ""
+
+def _exec_form (form):
+    global updatelog
+    log = _StringIO()
+    config = _Configuration()
+    doreload = False
+    try:
+        doreload = _update(config, BaseUrl, log=log, dryrun=True)
+        updatelog = log.getvalue()
+        config.write_filterconf()
+    except IOError, msg:
+        updatelog = "Error: %s" % msg
+    else:
+        if doreload:
+            from wc import daemon as _daemon
+            _daemon.reload()
+
