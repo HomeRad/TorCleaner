af3750f4ae7194e56159df4cd3b2dda4a79c9d72
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-08-11 16:11:03 +0000

    aded imagesize
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@1955 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ test/imagesize.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+"""download one image and try to guess its size"""
+import Image, sys
+from StringIO import StringIO
+try:
+    from wc.update import open_url
+    raise SystemExit("Global WebCleaner installation found")
+except ImportError:
+    import os
+    sys.path.insert(0, os.getcwd())
+    from wc.update import open_url
+
+def usage ():
+    print "usage: imagesize.py <url> [bufsize]"
+    sys.exit(1)
+
+def test ():
+    if len(sys.argv) < 2:
+        usage()
+    bufsize = 6000
+    url = sys.argv[1]
+    if len(sys.argv) > 3:
+        bufsize = int(sys.argv[2])
+    page = open_url(url)
+    buf = StringIO()
+    data = page.read()
+    print "downloaded", len(data), "bytes"
+    buf.write(data[:bufsize])
+    buf.seek(0)
+    img = Image.open(buf, 'r')
+
+
+if __name__=='__main__':
+    test()
