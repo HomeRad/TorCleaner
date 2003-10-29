343a04a91452dc9b8eef28bd66bc8e1d1cbc066e
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-10-29 15:17:29 +0000

    more adzapper parsing work
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@2272 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- config/adzap2wc.py
+++ config/adzap2wc.py
@@ -3,7 +3,7 @@
 # this script has to be executed in the config parent dir
 """Generate WebCleaner .zap files from an AdZapper squid_redirect file"""
 
-import sys, os, re
+import sys, os, re, time, gzip
 try:
     from wc.XmlUtils import xmlify
 except ImportError:
@@ -13,6 +13,9 @@ except ImportError:
 DOWNLOAD = "downloads"
 ADZAPPER_URL = "http://adzapper.sourceforge.net/scripts/squid_redirect"
 ADZAPPER_FILE = "%s/squid_redirect" % DOWNLOAD
+
+# used for .zap file timestamp
+date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 def download_adzapper_file ():
     """download adzapper url into temporary file and return filename"""
@@ -88,32 +91,80 @@ def remove (filename):
     os.unlink(filename)
 
 
-is_comment = re.compile('^\s*(#.*)?$').match
-
 def parse_adzapper_file (filename):
     res = {}
+    is_comment = re.compile('^\s*(#.*)?$').match
     content = False # skip content until __DATA__ marker
     for line in open(filename):
         if not content:
             content = line.startswith('__DATA__')
-        else:
-            if not is_comment(line):
-                parse_adzapper_line(line.strip(), res)
+        elif not is_comment(line):
+            parse_adzapper_line(line.strip(), res)
     return res
 
 
 def parse_adzapper_line (line, res):
     adclass, pattern = line.split(None, 1)
-    if " " in pattern:
-        pattern, replace = pattern.split(None, 1)
-    else:
-        replace = None
-    res.setdefault(adclass, []).append((pattern, replace))
+    res.setdefault(adclass.lower(), []).append(pattern)
+
+
+def open_files ():
+    filename = os.path.join("config", "adzapper_urls.gz")
+    if os.path.exists(filename):
+        remove(filename)
+    urlfile = gzip.GzipFile(filename, 'wb')
+    filename = os.path.join("config", "adzapper.zap")
+    if os.path.exists(filename):
+        remove(filename)
+    zapfile = file(filename, 'w')
+    d = {"title": xmlify("AdZapper filters"),
+         "desc": xmlify("Automatically generated on %s" % date),
+    }
+    zapfile.write("""<?xml version="1.0"?>
+<!DOCTYPE filter SYSTEM "filter.dtd">
+<folder title="%(title)s"
+ desc="%(desc)s"
+ disable="0">
+""" % d)
+    return urlfile, zapfile
+
+
+def close_files (urlfile, zapfile):
+    urlfile.close()
+    zapfile.write("</folder>\n")
+    zapfile.close()
 
 
 def write_filters (ads):
+    urlfile, zapfile = open_files()
+    for adclass, pattern in res.items():
+        pattern = convert_adzapper_pattern(pattern)
+        if adclass!='print':
+            replace = None
+            urlfile.write("%s\n" % pattern)
+        else:
+            pattern, replace = pattern.split(None, 1)
+            replace = convert_adzapper_replace(replace)
+        write_rewrite(zapfile, pattern, replace)
+    close_files(urlfile, zapfile)
+
+
+def convert_adzapper_pattern (pattern):
+    pattern = pattern.replace(".", "\\.")
+    pattern = pattern.replace("?", "\\?")
+    pattern = pattern.replace("**", ".*?")
+    pattern = re.sub(r"[^.]*[^?]", pattern, "[^/]*")
+    return pattern
+
+
+def convert_adzapper_replace (replace):
+    replace = re.sub(r"$(\d)", replace, r"\\1")
+    return replace
+
+
+def write_rewrite(zapfile, pattern, replacement):
     # XXX
-    print "write ads", ads
+    pass
 
 
 def download_and_parse ():
