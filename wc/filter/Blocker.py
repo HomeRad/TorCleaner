8df591cf505615fc305582601142c54c9ff9f7f7
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-11-08 20:37:40 +0000

    FilterException on CONNECT
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@2356 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- wc/filter/Blocker.py
+++ wc/filter/Blocker.py
@@ -20,7 +20,7 @@ __version__ = "$Revision$"[11:-2]
 __date__    = "$Date$"[7:-2]
 
 import re, urlparse, os, gzip
-from wc.filter import FILTER_REQUEST
+from wc.filter import FILTER_REQUEST, FilterException
 from wc.filter.Filter import Filter
 from wc import ConfigDir, config
 from wc.log import *
@@ -143,7 +143,11 @@ class Blocker (Filter):
                 # make HTTP HEAD request?
                 doc = self.block_url
             port = config['port']
-            return 'GET http://localhost:%d%s HTTP/1.1'%(port, doc)
+            newurl = 'http://localhost:%d%s' % (port, doc)
+            if method=='CONNECT':
+                # XXX we cannot handle https requests locally
+                raise FilterException(newurl)
+            return 'GET %s HTTP/1.1'%newurl
         return data
 
 
