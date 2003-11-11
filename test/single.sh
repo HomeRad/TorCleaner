46e66928c21035482262f98c8369ee13d1a7dd4c
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-11-11 21:00:18 +0000

    wait longer for proxy to start
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@2388 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/single.sh
+++ test/single.sh
@@ -4,6 +4,6 @@
 rm -f webcleaner.log* webcleaner.err
 env -u LANGUAGE -u LC_ALL -u LC_CTYPE -u LANG PYTHONPATH=`pwd` ./webcleaner restart 2> webcleaner.err
 if [ "x$1" != "x" ]; then
-    sleep 3
+    sleep 5
     env http_proxy="http://localhost:8080" wget "$1"
 fi
