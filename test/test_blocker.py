918c24619ed3b114cd01992546af3ad3282d1014
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-11-11 20:59:19 +0000

    pass complete request
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@2385 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/test_blocker.py
+++ test/test_blocker.py
@@ -3,14 +3,14 @@
 
 import wc, time
 from test_support import TestFailed
-reload(wc)
 url = "http://ads.realmedia.com/"
+data = "GET %s HTTP/1.0" % url
 from wc.log import initlog
 initlog("test/logging.conf")
 wc.config = wc.Configuration()
 wc.config.init_filter_modules()
 attrs = wc.filter.initStateObjects(url=url)
 filtered = \
-        wc.filter.applyfilter(wc.filter.FILTER_REQUEST, url, 'finish', attrs)
+      wc.filter.applyfilter(wc.filter.FILTER_REQUEST, data, 'finish', attrs)
 if filtered.find("blocked.html")==-1:
-    raise TestFailed, "unblocked query %s"%url
+    raise TestFailed, "unblocked query %s"%data
