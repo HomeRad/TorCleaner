7fa3b81f0993c37a85cf6cdb9d5255a797804400
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-11-11 21:47:17 +0000

    simplify, simplify
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@2390 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/single.sh
+++ test/single.sh
@@ -2,7 +2,7 @@
 # restart the webcleaner proxy, logging to local files
 # used for development test runs
 rm -f webcleaner.log* webcleaner.err
-env -u LANGUAGE -u LC_ALL -u LC_CTYPE -u LANG PYTHONPATH=`pwd` ./webcleaner restart 2> webcleaner.err
+test/run.sh webcleaner restart 2> webcleaner.err
 if [ "x$1" != "x" ]; then
     sleep 5
     env http_proxy="http://localhost:8080" wget "$1"
--- test/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-env -u LANGUAGE -u LC_ALL -u LC_CTYPE -u LANG PYTHONPATH=`pwd` python test/regrtest.py $*
