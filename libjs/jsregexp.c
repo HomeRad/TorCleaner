7e82b2b7fe43dc53bb4e1233696410fac2e114e0
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-11-16 22:40:17 +0000

    upstream cvs update
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@2425 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- libjs/jsregexp.c
+++ libjs/jsregexp.c
@@ -1,36 +1,41 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  *
- * The contents of this file are subject to the Netscape Public
- * License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/NPL/
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
  * The Original Code is Mozilla Communicator client code, released
  * March 31, 1998.
  *
- * The Initial Developer of the Original Code is Netscape
- * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation. All
- * Rights Reserved.
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 1998
+ * the Initial Developer. All Rights Reserved.
  *
- * Contributor(s): 
+ * Contributor(s):
  *
- * Alternatively, the contents of this file may be used under the
- * terms of the GNU Public License (the "GPL"), in which case the
- * provisions of the GPL are applicable instead of those above.
- * If you wish to allow use of your version of this file only
- * under the terms of the GPL and not to allow others to use your
- * version of this file under the NPL, indicate your decision by
- * deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL.  If you do not delete
- * the provisions above, a recipient may use your version of this
- * file under either the NPL or the GPL.
- */
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 /*
  * JS regular expressions, after Perl.
@@ -354,36 +359,36 @@ processOp(CompilerState *state, REOpData *opData, RENode **operandStack, intN op
             state->progLength += 13;
         }
         else
-            if ((((RENode *)(result->kid))->op == REOP_CLASS)
-                    && (((RENode *)(result->kid))->u.ucclass.index < 256)
-                    && (((RENode *)(result->u.kid2))->op == REOP_FLAT)
-                    && ((state->flags & JSREG_FOLD) == 0) ) {
-                result->op = REOP_ALTPREREQ2;
-                result->u.altprereq.ch1 
-                    = ((RENode *)(result->u.kid2))->u.flat.chr;
-                result->u.altprereq.ch2 
-                    = ((RENode *)(result->kid))->u.ucclass.index;
-                /* ALTPREREQ2, <end>, uch1, uch2, <next>, ..., 
-                                                JUMP, <end> ... ENDALT */
-                state->progLength += 13;
-            }
-            else
-                if ((((RENode *)(result->kid))->op == REOP_FLAT)
-                        && (((RENode *)(result->u.kid2))->op == REOP_CLASS)
-                        && (((RENode *)(result->u.kid2))->u.ucclass.index < 256)
-                        && ((state->flags & JSREG_FOLD) == 0) ) {
-                    result->op = REOP_ALTPREREQ2;
-                    result->u.altprereq.ch1 
-                        = ((RENode *)(result->kid))->u.flat.chr;
-                    result->u.altprereq.ch2 
-                        = ((RENode *)(result->u.kid2))->u.ucclass.index;
-                    /* ALTPREREQ2, <end>, uch1, uch2, <next>, ..., 
-                                                  JUMP, <end> ... ENDALT */
-                    state->progLength += 13;
-                }
-                else
-                    /* ALT, <next>, ..., JUMP, <end> ... ENDALT */
-                    state->progLength += 7;
+        if ((((RENode *)(result->kid))->op == REOP_CLASS)
+                && (((RENode *)(result->kid))->u.ucclass.index < 256)
+                && (((RENode *)(result->u.kid2))->op == REOP_FLAT)
+                && ((state->flags & JSREG_FOLD) == 0) ) {
+            result->op = REOP_ALTPREREQ2;
+            result->u.altprereq.ch1 
+                = ((RENode *)(result->u.kid2))->u.flat.chr;
+            result->u.altprereq.ch2 
+                = ((RENode *)(result->kid))->u.ucclass.index;
+            /* ALTPREREQ2, <end>, uch1, uch2, <next>, ..., 
+                                            JUMP, <end> ... ENDALT */
+            state->progLength += 13;
+        }
+        else
+        if ((((RENode *)(result->kid))->op == REOP_FLAT)
+                && (((RENode *)(result->u.kid2))->op == REOP_CLASS)
+                && (((RENode *)(result->u.kid2))->u.ucclass.index < 256)
+                && ((state->flags & JSREG_FOLD) == 0) ) {
+            result->op = REOP_ALTPREREQ2;
+            result->u.altprereq.ch1 
+                = ((RENode *)(result->kid))->u.flat.chr;
+            result->u.altprereq.ch2 
+                = ((RENode *)(result->u.kid2))->u.ucclass.index;
+            /* ALTPREREQ2, <end>, uch1, uch2, <next>, ..., 
+                                          JUMP, <end> ... ENDALT */
+            state->progLength += 13;
+        }
+        else
+            /* ALT, <next>, ..., JUMP, <end> ... ENDALT */
+            state->progLength += 7;
         break;
     case REOP_CONCAT:
         result = operandStack[operandSP - 2];
@@ -2355,30 +2360,24 @@ executeREBytecode(REGlobalData *gData, REMatchState *x)
                 pc += ARG_LEN;
 
                 if (x->cp != gData->cpend) {
+                    if (*x->cp == matchCh2)
+                        goto doAlt;
+
                     charSet = &gData->regexp->classList[k];
                     if (!charSet->converted)
                         if (!processCharSet(gData, charSet))
                             return JS_FALSE;
                     matchCh1 = *x->cp;
                     k = matchCh1 / 8;
-                    if ((charSet->length != 0) && 
-                           ( (matchCh1 <= charSet->length)
-                           && ((charSet->u.bits[k] 
-                                    & (1 << (matchCh1 & 0x7))) != 0) )) {
-                        result = NULL;
-                        break;
+                    if ((charSet->length == 0 ||
+                         matchCh1 > charSet->length ||
+                         (charSet->u.bits[k] & (1 << (matchCh1 & 0x7))) == 0)
+                        ^ charSet->sense) {
+                        goto doAlt;
                     }
                 }
-                else {
-                    result = NULL;
-                    break;
-                }
-
-                if ((x->cp == gData->cpend) || (*x->cp != matchCh2)) {
-                    result = NULL;
-                    break;
-                }
-                goto doAlt;
+                result = NULL;
+                break;
 
             case REOP_ALTPREREQ:
                 nextpc = pc + GET_OFFSET(pc);   /* start of next op */
@@ -3452,7 +3451,8 @@ regexp_toString(JSContext *cx, JSObject *obj, uintN argc, jsval *argv,
                 jsval *rval)
 {
     JSRegExp *re;
-    jschar *source, *chars;
+    const jschar *source;
+    jschar *chars;
     size_t length, nflags;
     uintN flags;
     JSString *str;
