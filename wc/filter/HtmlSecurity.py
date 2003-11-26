62c34523c3789607f9fbf5117278c6fd88d6407a
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-11-26 21:27:39 +0000

    restructured
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@2475 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- wc/filter/HtmlSecurity.py
+++ wc/filter/HtmlSecurity.py
@@ -23,20 +23,15 @@ import os, sys, re
 from wc.log import *
 
 _percent_encodings = re.compile('%+').findall
-def _has_lots_of_percents (href):
-    for match in _percent_encodings(href):
+def _has_lots_of_percents (url):
+    for match in _percent_encodings(url):
         if len(match) > 10:
             return True
     return False
 
 
 class HtmlSecurity (object):
-    """Scan and repair known security exploits in HTML start/end tags.
-       XXX would be fixed with file(1) like scanning of documents and
-       fixing of the content-type header value:
-         CVE-2002-0025
-         CVE-2002-0024
-       """
+    """Scan and repair known security exploits in HTML start/end tags."""
     def __init__ (self):
         self.in_winhelp = False # inside object tag calling WinHelp
         # running on MacOS or MacOSX
@@ -45,22 +40,43 @@ class HtmlSecurity (object):
 
 
     def scan_start_tag (self, tag, attrs, htmlfilter):
-        if tag=="input" and attrs.has_key('type'):
+        fun = "%s_start"%tag
+        if hasattr(self, fun):
+            getattr(self, fun)(attrs, htmlfilter)
+
+
+    def scan_end_tag (self, tag):
+        fun = "%s_end"%tag
+        if hasattr(self, fun):
+            getattr(self, fun)(htmlfilter)
+
+
+    def input_start (self, attrs, htmlfilter):
+        if attrs.has_key('type'):
             # prevent IE crash bug on empty type attribute
             if not attrs['type']:
                 warn(FILTER, "%s\n Detected and prevented IE <input type> crash bug", str(htmlfilter))
                 del attrs['type']
-        elif tag=="fieldset" and attrs.has_key('style'):
+
+
+    def fieldset_start (self, attrs, htmlfilter):
+        if attrs.has_key('style'):
             # prevent Mozilla crash bug on fieldsets
             if "position" in attrs['style']:
                 warn(FILTER, "%s\n Detected and prevented Mozilla <fieldset style> crash bug", str(htmlfilter))
                 del attrs['style']
-        elif tag=="hr" and attrs.has_key('align'):
+
+
+    def hr_start (self, attrs, htmlfilter):
+        if attrs.has_key('align'):
             # prevent CAN-2003-0469, length 50 should be safe
             if len(attrs['align']) > 50:
                 warn(FILTER, "%s\n Detected and prevented IE <hr align> crash bug", str(htmlfilter))
                 del attrs['align']
-        elif tag=="object" and attrs.has_key('type'):
+
+
+    def object_start (self, attrs, htmlfilter):
+        if attrs.has_key('type'):
             # prevent CAN-2003-0344, only one / (slash) allowed
             t = attrs['type']
             c = t.count("/")
@@ -68,19 +84,36 @@ class HtmlSecurity (object):
                 warn(FILTER, "%s\n Detected and prevented IE <object type> bug", str(htmlfilter))
                 t = t.replace("/", "", c-1)
                 attrs['type'] = t
-        elif tag=='table' and attrs.has_key('width'):
+
+
+    def table_start (self, attrs, htmlfilter):
+        if attrs.has_key('width'):
             # prevent CAN-2003-0238, table width=-1 crashes ICQ client
             if attrs['width']=='-1':
                 warn(FILTER, "%s\n Detected and prevented ICQ table width crash bug", str(htmlfilter))
                 del attrs['width']
-        elif tag=='object' and attrs.has_key('codebase'):
+
+
+    def object_start (self, attrs, htmlfilter):
+        if attrs.has_key('codebase'):
             self.in_winhelp = attrs['codebase'].lower().startswith('hhctrl.ocx')
-        elif tag=='param' and attrs.has_key('value') and self.in_winhelp:
+
+
+    def object_end (self, htmlfilter):
+        if self.in_winhelp:
+            self.in_winhelp = False
+
+
+    def param_start (self, attrs, htmlfilter):
+        if attrs.has_key('value') and self.in_winhelp:
             # prevent CVE-2002-0823
             if len(attrs['value']) > 50:
                 warn(FILTER, "%s\n Detected and prevented WinHlp overflow bug", str(htmlfilter))
                 del attrs['value']
-        elif tag=='meta' and attrs.has_key('content') and self.macintosh:
+
+
+    def meta_start (self, attrs, htmlfilter):
+        if attrs.has_key('content') and self.macintosh:
             # prevent CVE-2002-0153
             if attrs.get('http-equiv', '').lower()=='refresh':
                 url = attrs['content'].lower()
@@ -91,7 +124,10 @@ class HtmlSecurity (object):
                 if url.startswith('file:/'):
                     warn(FILTER, "%s %s\n Detected and prevented local file redirection", str(htmlfilter), `attrs['content']`)
                     del attrs['content']
-        elif tag=='embed' and attrs.has_key('src'):
+
+
+    def embed_start (self, attrs, htmlfilter):
+        if attrs.has_key('src'):
             src = attrs['src']
             if "." in src:
                 # prevent CVE-2002-0022
@@ -99,19 +135,25 @@ class HtmlSecurity (object):
                 if len(src[i:]) > 10:
                     warn(FILTER, "%s %s\n Detected and prevented IE filename overflow crash", str(htmlfilter), `src`)
                     del attrs['src']
-        elif tag=='font' and attrs.has_key('size'):
+
+
+    def font_start (self, attrs, htmlfilter):
+        if attrs.has_key('size'):
             if len(attrs['size']) > 10:
                 # prevent CVE-2001-0130
                 warn(FILTER, "%s %s\n Detected and prevented Lotus Domino font size overflow crash", str(htmlfilter), `attrs['size']`)
                 del attrs['size']
-        elif tag=='a' and attrs.has_key('href'):
-            href = attrs['href']
-            if _has_lots_of_percents(href):
+
+
+    def a_start (self, attrs, htmlfilter):
+        self.check_percent_url(attrs, 'href', htmlfilter)
+
+
+    def check_percent_url (self, attrs, name, htmlfilter):
+        if attrs.has_key(name):
+            url = attrs[name]
+            if _has_lots_of_percents(url):
                 # prevent CAN-2003-0870
-                warn(FILTER, "%s %s\n Detected and prevented Opera percent encoding overflow crash", str(htmlfilter), `href`)
-                del attrs['href']
+                warn(FILTER, "%s %s\n Detected and prevented Opera percent encoding overflow crash", str(htmlfilter), `url`)
+                del attrs[name]
 
-
-    def scan_end_tag (self, tag):
-        if self.in_winhelp and tag=='object':
-            self.in_winhelp = False
