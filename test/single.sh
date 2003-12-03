88005977c662b3c1d93c9dfe79411ff649886b9c
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2003-12-03 21:50:41 +0000

    use the watch daemon
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@2504 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/single.sh
+++ test/single.sh
@@ -2,7 +2,8 @@
 # restart the webcleaner proxy, logging to local files
 # used for development test runs
 rm -f webcleaner.log* webcleaner.err
-test/run.sh webcleaner restart 2> webcleaner.err
+test/run.sh webcleaner stopwatch
+test/run.sh webcleaner startwatch 2> webcleaner.err
 if [ "x$1" != "x" ]; then
     sleep 5
     env http_proxy="http://localhost:8080" wget "$1"
