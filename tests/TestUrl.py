5c1dc0ab0fa1c71e1215f908d29fb90c91bb800b
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-02-22 22:27:24 +0000

    unit test
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@2897 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ tests/TestUrl.py
@@ -0,0 +1,15 @@
+# -*- coding: iso-8859-1 -*-
+import unittest
+from wc.url import url_norm, url_quote
+
+class TestUrl (unittest.TestCase):
+    def testUrl (self):
+        url = "http://server/..%5c..%5c..%5c..%5c..%5c..%5..%5c..%5ccskin.zip"
+        nurl = url_quote(url_norm(url))
+        self.assertEquals(nurl, "http://server/cskin.zip")
+        url = "http://groups.google.com/groups?hl=en&lr=&ie=UTF-8&threadm=3845B54D.E546F9BD%40monmouth.com&rnum=2&prev=/groups%3Fq%3Dlogitech%2Bwingman%2Bextreme%2Bdigital%2B3d%26hl%3Den%26lr%3D%26ie%3DUTF-8%26selm%3D3845B54D.E546F9BD%2540monmouth.com%26rnum%3D2"
+        nurl = url_quote(url_norm(url))
+        self.assertEqual(url, nurl)
+
+if __name__ == '__main__':
+    unittest.main()
