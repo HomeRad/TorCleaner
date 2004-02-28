8cd958ed58a03d87db6294e1b578a95ce8c91b66
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-02-28 16:39:58 +0000

    moved
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@3007 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ tests/TestUpdate.py
@@ -0,0 +1,22 @@
+# -*- coding: iso-8859-1 -*-
+import unittest
+import wc
+from wc.log import *
+from wc.update import update_filter, update_ratings
+
+class TestUpdate (unittest.TestCase):
+    def setUp (self):
+        initlog("test/logging.conf")
+        self.config = wc.Configuration()
+
+
+    def testUpdateFilter (self):
+        update_filter(config, dryrun=True)
+
+
+    def testUpdateRatings (self):
+        update_ratings(config, dryrun=True)
+
+
+if __name__ == '__main__':
+    unittest.main()
