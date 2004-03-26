68eee8aed48e760edc6c66d558194f8a035259a8
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-03-26 13:09:38 +0000

    updated tests
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@3198 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/regrtest.py
+++ test/regrtest.py
@@ -129,10 +129,6 @@ def main(tests=None, testdir=None, verbose=0, quiet=0, generate=0,
     return len(bad) > 0
 
 STDTESTS = [
-    'test_js',
-    'test_rating',
-    'test_replacer',
-    'test_rewriter',
     'test_script',
     'test_simpletal',
    ]
