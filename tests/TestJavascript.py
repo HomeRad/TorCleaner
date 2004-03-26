5c24a11ab26715567d584e92910ecd22bb793d47
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-03-26 09:40:16 +0000

    added
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@3191 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ tests/TestJavascript.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+"""test script to test JavaScript engine"""
+
+import unittest
+from wc.js import jslib, JSListener
+
+class JSTester (JSListener.JSListener):
+    """JavaScript engine event listener"""
+    def __init__ (self):
+        self.data = ""
+        self.popups = 0
+        self.err = ""
+
+
+    def jsProcessData (self, data):
+        self.data += data
+
+
+    def jsProcessPopup (self):
+        self.popups += 1
+
+
+    def jsProcessError (self, err):
+        self.err = err
+
+
+class TestJavaScript (unittest.TestCase):
+    def setUp (self):
+        self.out = JSTester()
+        self.jsEnv = jslib.JSEnv()
+        self.jsEnv.listeners.append(self.out)
+
+
+    def testPopups (self):
+        """test popup counting"""
+        self.jsEnv.executeScript("window.open('')", 1.1)
+        self.jsEnv.executeScript("eval('window.open(\\'\\')')", 1.1)
+        self.assert_(self.out.popups==2)
+
+
+    def testDocumentWrite (self):
+        """document.write() function"""
+        self.jsEnv.executeScript("""
+a='ht'
+b='ml'
+document.write('<'+a+b+'>')""", 1.1)
+        self.assert_(self.out.data=="<html>")
+
+
+    def testReferenceError (self):
+        """test provoked reference error"""
+        self.jsEnv.executeScript("ä", 1.1)
+        self.assert_(self.out.err.startswith("ReferenceError:"))
+
+
+    def testSyntaxError (self):
+        """test provoked syntax error"""
+        self.jsEnv.executeScript("a='", 1.5)
+        self.assert_(self.out.err.startswith("SyntaxError:"))
+
+
+    def tearDown (self):
+        """remove js engine listener"""
+        self.jsEnv.listeners.remove(self.out)
+        self.jsEnv = None
+
+
+if __name__ == '__main__':
+    unittest.main()
