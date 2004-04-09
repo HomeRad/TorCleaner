671484415c63df80e2cede7c9caad10714d7175a
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-04-09 11:30:27 +0000

    suppress logging
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@3305 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- tests/TestUpdate.py
+++ tests/TestUpdate.py
@@ -4,18 +4,24 @@ import wc
 from wc.log import *
 from wc.update import update_filter, update_ratings
 
+class NoLog (object):
+    def write (self, s):
+        pass
+
+
 class TestUpdate (unittest.TestCase):
     def setUp (self):
+        self.nolog = NoLog()
         initlog("test/logging.conf")
         self.config = wc.Configuration()
 
 
     def testUpdateFilter (self):
-        update_filter(self.config, dryrun=True)
+        update_filter(self.config, dryrun=True, log=self.nolog)
 
 
     def testUpdateRatings (self):
-        update_ratings(self.config, dryrun=True)
+        update_ratings(self.config, dryrun=True, log=self.nolog)
 
 
 suite = unittest.makeSuite(TestUpdate,'test')
