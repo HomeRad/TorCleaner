1e99dc19bb4a2b64e4f68a987764980c30213b0f
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-04-16 16:29:08 +0000

    documentation
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@3355 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- tests/TestRewriter.py
+++ tests/TestRewriter.py
@@ -25,41 +25,55 @@ class TestRewriter (StandardTest):
         self.attrs = get_filterattrs("", [FILTER_RESPONSE_MODIFY], headers=self.headers)
 
     def filt (self, data, result):
+        """filter specified data, expect result. Call this only once per
+           test!"""
         filtered = applyfilter(FILTER_RESPONSE_MODIFY, data, 'finish', self.attrs)
         self.assertEqual(filtered, result)
 
     def testClosingTag (self):
+        """close tag in attribute value"""
         self.filt("""<a CONTENT="Andrew McDonald <andrew@mcdonald.org.uk>">""",
                   """<a content="Andrew McDonald <andrew@mcdonald.org.uk>">""")
 
     def testMetaRefresh (self):
+        """meta refresh"""
         self.filt("""<META http-equiv="refresh">""",
                   """<meta http-equiv="refresh">""")
 
-    def testMetaReFresh (self):
+    def testMetaRefresh2 (self):
+        """meta reFresh"""
         self.filt("""<meta http-equiv="ReFresh">""",
                   """<meta http-equiv="ReFresh">""")
 
     def testMetaRefrish (self):
+        """meta refrish"""
         self.filt("""<meta http-equiv="Refrish">""",
                   """<meta http-equiv="Refrish">""")
 
     def testShortcutIcon (self):
+        """shortcut icon"""
         self.filt("""<link rel="shortcut icon"></link>""", "")
 
     def testJavascriptInBody (self):
+        """onunload in body"""
         self.filt("""<body onload="hulla();" onunload="holla();">""",
                   """<body onload="hulla();">""")
 
-    def testAdvertLinks (self):
+    def testAdvertLinks1 (self):
+        """Doubleclick advert"""
         self.filt("""<a href="http://www.doubleclick.net/">...</a>""", "")
+
+    def testAdvertLinks2 (self):
+        """freshmeat ad"""
         self.filt("""<a href="http://ads.freshmeat.net/">...</a>""", "")
 
     def testBlink (self):
+        """blinking text"""
         self.filt("""<blink>blinking text</blink>""",
                   """<b>blinking text</b>""")
 
     def testNoscript (self):
+        """remove noscript"""
         self.filt("""<noscript>Kein Javascript</noscript>""", "")
 
     def XXXtestErotic (self):
@@ -67,22 +81,28 @@ class TestRewriter (StandardTest):
                   """<a href="http://www.calvinandhobbes.com/">blubba</a>""")
 
     def testRedirect (self):
+        """fileleech redirection"""
         self.filt("""<a href="http://www.fileleech.com/dl/?filepath=http://www.counter-strike.de/downloads/ghl10full.exe&download=1">CS 1.0</a>""",
                   """<a href="http://www.counter-strike.de/downloads/ghl10full.exe">CS 1.0</a>""")
 
     def testEntity (self):
+        """lone entity quoting"""
         self.filt("""Hallo Ernie & Bert, was geht?""",
                   """Hallo Ernie & Bert, was geht?""")
 
     def testEntities (self):
+        """entity quoting"""
         self.filt("""Hallo Ernie &amp; Bert, was geht &lt;ab&gt;&#183;הצü?""",
                   """Hallo Ernie &amp; Bert, was geht &lt;ab&gt;&#183;הצü?""")
 
     def testTrackerImage (self):
+        """1x1 tracker image"""
         self.filt("""<img src="blubb" width="1" height="1">""", "")
 
     def testAdlog (self):
-        self.filt("""<a href='http://fmads.osdn.com/cgi-bin/adlog.pl?index,tkgk0128en'></a>""", "")
+        """adlog.pl advert"""
+        self.filt("""<a href='http://fmads.osdn.com/cgi-bin/adlog.pl?index,tkgk0128en'></a>""",
+                  "")
 
     def testStartTag (self):
         """unquoted attribute ending with slash"""
@@ -90,16 +110,17 @@ class TestRewriter (StandardTest):
                   """<a href="http://www/">link</a>""")
 
     def testUncommonAttrChars (self):
+        """uncommon attribute characters"""
         self.filt("""<a href="123$456">abc</a>""",
                   """<a href="123$456">abc</a>""")
 
     def testInputType (self):
-        """IE crash"""
+        """IE input type crash"""
         self.filt("""<input type >""",
                   """<input>""")
 
     def testFielsetStyle (self):
-        """Mozilla crash"""
+        """Mozilla fieldset crash"""
         self.filt("""<fieldset style="position:absolute;">""",
                   """<fieldset>""")
 
@@ -109,12 +130,12 @@ class TestRewriter (StandardTest):
                   """<hr>""")
 
     def testObjectType (self):
-        """IE crash"""
+        """IE object type crash"""
         self.filt("""<object type="////////////////////////////////////////////////////////////AAAAA">""",
                   """<object type="/AAAAA">""")
 
     def testHrefPercent (self):
-        """Opera crash"""
+        """Opera file crash"""
         self.filt("""<a href="file://server%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%text"></a>""",
                   """<a></a>""")
 
