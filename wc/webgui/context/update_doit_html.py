7f7ab13d2ba6b1cf077dced3b7133ac6ab18a065
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-04-20 21:31:04 +0000

    fix _update_filter import
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@3375 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- wc/webgui/context/update_doit_html.py
+++ wc/webgui/context/update_doit_html.py
@@ -20,7 +20,7 @@ __version__ = "$Revision$"[11:-2]
 __date__    = "$Date$"[7:-2]
 
 from wc import AppName, Version, config
-from wc.update import update as _update
+from wc.update import update_filter as _update_filter
 from wc.update import update_ratings as _update_ratings
 from cStringIO import StringIO as _StringIO
 
