a616caffa50a0813243b0fa91609fed6cd6890bd
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-04-21 20:00:47 +0000

    added
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@3394 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ tests/StandardTest.py
@@ -0,0 +1,25 @@
+# -*- coding: iso-8859-1 -*-
+"""basic test class"""
+
+import unittest, os
+from test import initlog
+
+class StandardTest (unittest.TestCase, object):
+    """A class augmenting the default unittest TestCase by init() and
+       shutdown() methods, each called only once before and after
+       execution of all test methods.
+    """
+    def __call__(self, result=None):
+        self.showAll = result.showAll
+        self.init()
+        try:
+            return super(StandardTest, self).__call__(result=result)
+        finally:
+            self.shutdown()
+
+    def init (self):
+        """base initialization of the logging framework"""
+        initlog(os.path.join("test", "logging.conf"))
+
+    def shutdown (self):
+        pass
