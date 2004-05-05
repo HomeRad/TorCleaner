074b079470447390f618e8f9b7cd7c1f95c885b5
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-05-05 21:02:41 +0000

    removed
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@3514 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- diff.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PYTHON=/usr/bin/python2.3
-$PYTHON webcleaner restart
-sleep 4
-env http_proxy="" wget -t1 -O unfiltered.html "$1"
-env http_proxy="http://localhost:8080" wget -t1 -O filtered.html "$1"
-#diff -BuN unfiltered.html filtered.html
-#rm -f unfiltered.html filtered.html
