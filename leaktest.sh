c6f61695d067572b30d78d522c37b63abce3326e
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-05-05 21:38:19 +0000

    updated
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@3519 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- leaktest.sh
+++ leaktest.sh
@@ -2,7 +2,7 @@
 # install valgrind to run this script
 #F=test/html/script2.html
 #valgrind --leak-check=yes test/filterfile.py $F 2>/dev/stdout | tee leak.log
-wget --delete-after http://localhost:8080/
-wget --delete-after http://localhost:8080/
-wget --delete-after http://localhost:8080/
-wget --delete-after http://localhost:8080/
+wget --delete-after http://localhost:8081/
+wget --delete-after http://localhost:8081/
+wget --delete-after http://localhost:8081/
+wget --delete-after http://localhost:8081/
