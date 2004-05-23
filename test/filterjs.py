742fc835314812afb2d6c0b4bf579853fc175d60
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-05-23 18:40:19 +0000

    added
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@3648 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ test/filterjs.py
@@ -0,0 +1,78 @@
+# -*- coding: iso-8859-1 -*-
+"""filter JavaScript."""
+# Copyright (C) 2000-2004  Bastian Kleineidam
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+__version__ = "$Revision$"[11:-2]
+__date__    = "$Date$"[7:-2]
+
+import wc
+import wc.js
+import wc.js.jslib
+import wc.js.JSListener
+import wc.filter.rules.RewriteRule
+
+
+class JSFilter (wc.js.JSListener.JSListener):
+    """defines callback handlers for filtering Javascript code"""
+
+    def __init__ (self, script, ver):
+        self.js_env = wc.js.jslib.JSEnv()
+        self.js_env.listeners.append(self)
+        self.js_env.executeScript(wc.js.unescape_js(script), ver)
+        self.js_env.listeners.remove(self)
+
+    def _str__ (self):
+        return self.__class__.__name__
+
+    def jsProcessData (self, data):
+        """produced by document.write() JavaScript"""
+        print "jsProcessData", repr(data)
+
+    def jsProcessPopup (self):
+        """process javascript popup"""
+        print "jsProcessPopup"
+
+    def jsProcessError (self, msg):
+        """process javascript syntax error"""
+        print "jsProcessError", msg
+
+
+def _main ():
+    """USAGE: test/run.sh test/filterjs.py <.js file>"""
+    import sys
+    if len(sys.argv)!=2:
+        print _main.__doc__
+        sys.exit(1)
+    fname = sys.argv[1]
+    if fname=="-":
+        f = sys.stdin
+    else:
+        f = file(fname)
+    from test import initlog, disable_rating_rules
+    initlog("test/logging.conf")
+    import wc
+    wc.config = wc.Configuration()
+    disable_rating_rules(wc.config)
+    wc.config['filters'] = ['Replacer', 'Rewriter', 'BinaryCharFilter']
+    wc.config.init_filter_modules()
+    script = f.read()
+    ver = 1.1
+    JSFilter(script, ver)
+
+
+if __name__=='__main__':
+    _main()
