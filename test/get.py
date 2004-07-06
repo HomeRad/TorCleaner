fa48b33ec732474607a5dc88619329aa2acd2d9b
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-07-06 21:57:15 +0000

    debug level
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@3814 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/get.py
+++ test/get.py
@@ -14,6 +14,7 @@ def _main ():
     host = parts[1]
     path = urlparse.urlunsplit(('', '', parts[2], parts[3], parts[4]))
     h = httplib.HTTPConnection(host)
+    h.set_debuglevel(1)
     h.connect()
     h.putrequest("GET", path, skip_host=1)
     h.putheader("Host", host)
