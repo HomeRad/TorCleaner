1b0f6d85d9adcceef64f7cef980cba31ebce0242
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-07-14 15:19:25 +0000

    added tests
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@3824 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ wc/proxy/auth/tests/test_digest.py
@@ -0,0 +1,17 @@
+# -*- coding: iso-8859-1 -*-
+import unittest
+import wc.proxy.auth
+
+class TestDigest (unittest.TestCase):
+    """Test authentication routines"""
+
+    def test_digest (self):
+        """construct and parse Digest authentication messages"""
+        chal = 'Digest realm="This is my digest auth", nonce="i5tbP9h2RAiGbccW", qop="auth", stale=false'
+        chals = wc.proxy.auth.parse_challenges(chal)
+        attrs = {"username": "calvin", "password_b64": "Y2Fsdmlu",
+                 "uri": "/logo.gif", "method": "GET",
+                 "requireExtraQuotes": False}
+        creds = wc.proxy.auth.parse_credentials(wc.proxy.auth.get_credentials(chals, **attrs))
+        #wccreds = creds['Digest'][0]
+        # XXX test this data
---
+++ wc/proxy/auth/tests/test_ntlm.py
@@ -0,0 +1,52 @@
+# -*- coding: iso-8859-1 -*-
+import unittest
+import base64
+import wc
+import wc.proxy.auth
+import StandardTest
+
+class TestNtlm (unittest.TestCase):
+    """Test ntlm authentication routines"""
+
+    def test_ntlm (self):
+        """construct and parse NTLM authentication messages"""
+        wc.config['auth_ntlm'] = 1
+        # challenge type 0
+        challenges = wc.proxy.auth.get_challenges(type=wc.proxy.auth.ntlm.NTLMSSP_INIT)
+        challenges = wc.proxy.auth.parse_challenges(", ".join(challenges))
+        self.assertEqual(challenges['NTLM'][0]['type'], 0)
+        # credentials type 1
+        attrs = {"username": "calvin", "type": wc.proxy.auth.ntlm.NTLMSSP_NEGOTIATE,}
+        creds = wc.proxy.auth.get_credentials(challenges, **attrs)
+        creds = wc.proxy.auth.parse_credentials(creds)
+        self.assertEqual(creds['NTLM'][0]['type'], 1)
+        # challenges type 2
+        attrs['host'] = creds['NTLM'][0]['host']
+        attrs['domain'] = creds['NTLM'][0]['domain']
+        attrs['type'] = wc.proxy.auth.ntlm.NTLMSSP_CHALLENGE
+        challenges = wc.proxy.auth.get_challenges(**attrs)
+        challenges = wc.proxy.auth.parse_challenges(", ".join(challenges))
+        self.assertEqual(challenges['NTLM'][0]['type'], 2)
+        # credentials type 3
+        attrs['type'] = wc.proxy.auth.ntlm.NTLMSSP_AUTH
+        attrs['nonce'] = challenges['NTLM'][0]['nonce']
+        attrs['password_b64'] = base64.encodestring("Beeblebrox")
+        creds = wc.proxy.auth.get_credentials(challenges, **attrs)
+        creds = wc.proxy.auth.parse_credentials(creds)
+        self.assertEqual(creds['NTLM'][0]['type'], 3)
+        self.assertEqual(creds['NTLM'][0]['username'], 'calvin')
+        self.assert_(wc.proxy.auth.check_credentials(creds, **attrs))
+        wc.config['auth_ntlm'] = 0
+
+    def test_ntlmpass (self):
+        """construct NTLM hashed password responses"""
+        password = "Beeblebrox"
+        nonce = "SrvNonce"
+        lm_hashed_pw = wc.proxy.auth.ntlm.create_lm_hashed_password(password)
+        nt_hashed_pw = wc.proxy.auth.ntlm.create_nt_hashed_password(password)
+        correct_lm_resp = "\xad\x87\xca\x6d\xef\xe3\x46\x85\xb9\xc4\x3c\x47\x7a\x8c\x42\xd6\x00\x66\x7d\x68\x92\xe7\xe8\x97"
+        correct_nt_resp = "\xe0\xe0\x0d\xe3\x10\x4a\x1b\xf2\x05\x3f\x07\xc7\xdd\xa8\x2d\x3c\x48\x9a\xe9\x89\xe1\xb0\x00\xd3"
+        lm_resp = wc.proxy.auth.ntlm.calc_resp(lm_hashed_pw, nonce)
+        nt_resp = wc.proxy.auth.ntlm.calc_resp(nt_hashed_pw, nonce)
+        self.assertEqual(correct_lm_resp, lm_resp)
+        self.assertEqual(correct_nt_resp, nt_resp)
