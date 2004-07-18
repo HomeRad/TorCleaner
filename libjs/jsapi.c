cfc5bfc41165e3f85ca7f734a54bbb6dbcbd6d03
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-07-18 20:59:30 +0000

    upstream CVS updated
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@3845 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- libjs/jsapi.c
+++ libjs/jsapi.c
@@ -2465,6 +2465,23 @@ JS_SetPropertyAttributes(JSContext *cx, JSObject *obj, const char *name,
 }
 
 JS_PUBLIC_API(JSBool)
+JS_HasProperty(JSContext *cx, JSObject *obj, const char *name, JSBool *foundp)
+{
+    JSBool ok;
+    JSObject *obj2;
+    JSProperty *prop;
+
+    CHECK_REQUEST(cx);
+    ok = LookupProperty(cx, obj, name, &obj2, &prop);
+    if (ok) {
+        *foundp = (prop != NULL);
+        if (prop)
+            OBJ_DROP_PROPERTY(cx, obj2, prop);
+    }
+    return ok;
+}
+
+JS_PUBLIC_API(JSBool)
 JS_LookupProperty(JSContext *cx, JSObject *obj, const char *name, jsval *vp)
 {
     JSBool ok;
@@ -2567,6 +2584,25 @@ JS_DefineUCPropertyWithTinyId(JSContext *cx, JSObject *obj,
     CHECK_REQUEST(cx);
     return DefineUCProperty(cx, obj, name, namelen, value, getter, setter,
                             attrs, SPROP_HAS_SHORTID, tinyid);
+}
+
+JS_PUBLIC_API(JSBool)
+JS_HasUCProperty(JSContext *cx, JSObject *obj,
+                 const jschar *name, size_t namelen,
+                 JSBool *vp)
+{
+    JSBool ok;
+    JSObject *obj2;
+    JSProperty *prop;
+
+    CHECK_REQUEST(cx);
+    ok = LookupUCProperty(cx, obj, name, namelen, &obj2, &prop);
+    if (ok) {
+        *vp = (prop != NULL);
+        if (prop)
+            OBJ_DROP_PROPERTY(cx, obj2, prop);
+    }
+    return ok;
 }
 
 JS_PUBLIC_API(JSBool)
@@ -2701,6 +2737,23 @@ JS_AliasElement(JSContext *cx, JSObject *obj, const char *name, jsint alias)
                                sprop->shortid)
           != NULL);
     OBJ_DROP_PROPERTY(cx, obj, prop);
+    return ok;
+}
+
+JS_PUBLIC_API(JSBool)
+JS_HasElement(JSContext *cx, JSObject *obj, jsint index, JSBool *foundp)
+{
+    JSBool ok;
+    JSObject *obj2;
+    JSProperty *prop;
+
+    CHECK_REQUEST(cx);
+    ok = OBJ_LOOKUP_PROPERTY(cx, obj, INT_TO_JSVAL(index), &obj2, &prop);
+    if (ok) {
+        *foundp = (prop != NULL);
+        if (prop)
+            OBJ_DROP_PROPERTY(cx, obj2, prop);
+    }
     return ok;
 }
 
@@ -3472,8 +3525,15 @@ JS_ExecuteScript(JSContext *cx, JSObject *obj, JSScript *script, jsval *rval)
     CHECK_REQUEST(cx);
     if (!js_Execute(cx, obj, script, NULL, 0, rval)) {
 #if JS_HAS_EXCEPTIONS
-        if (!cx->fp)
+        if (!cx->fp) {
+            JSStackFrame frame;
+            memset(&frame, 0, sizeof frame);
+            frame.script = script;
+            frame.pc = script->main;
+            cx->fp = &frame;
             js_ReportUncaughtException(cx);
+            cx->fp = NULL;
+        }
 #endif
         return JS_FALSE;
     }
@@ -3581,8 +3641,15 @@ JS_EvaluateUCScriptForPrincipals(JSContext *cx, JSObject *obj,
         return JS_FALSE;
     ok = js_Execute(cx, obj, script, NULL, 0, rval);
 #if JS_HAS_EXCEPTIONS
-    if (!ok && !cx->fp)
+    if (!ok && !cx->fp) {
+        JSStackFrame frame;
+        memset(&frame, 0, sizeof frame);
+        frame.script = script;
+        frame.pc = script->main;
+        cx->fp = &frame;
         js_ReportUncaughtException(cx);
+        cx->fp = NULL;
+    }
 #endif
     JS_DestroyScript(cx, script);
     return ok;
@@ -3630,8 +3697,19 @@ JS_CallFunctionValue(JSContext *cx, JSObject *obj, jsval fval, uintN argc,
     CHECK_REQUEST(cx);
     if (!js_InternalCall(cx, obj, fval, argc, argv, rval)) {
 #if JS_HAS_EXCEPTIONS
-        if (!cx->fp)
+        if (!cx->fp) {
+            JSStackFrame frame;
+            JSFunction *fun = JS_ValueToFunction(cx, fval);
+            memset(&frame, 0, sizeof frame);
+            if (fun) {
+                JSScript *script = fun->u.script;
+                frame.script = script;
+                frame.pc = script->main;
+                cx->fp = &frame;
+            }
             js_ReportUncaughtException(cx);
+            cx->fp = NULL;
+        }
 #endif
         return JS_FALSE;
     }
@@ -4108,6 +4186,17 @@ JS_ClearPendingException(JSContext *cx)
 #if JS_HAS_EXCEPTIONS
     cx->throwing = JS_FALSE;
     cx->exception = JSVAL_VOID;
+#endif
+}
+
+JS_PUBLIC_API(JSBool)
+JS_ReportPendingException(JSContext *cx)
+{
+#if JS_HAS_EXCEPTIONS
+    CHECK_REQUEST(cx);
+    return js_ReportUncaughtException(cx);
+#else
+    return JS_TRUE;
 #endif
 }
 
