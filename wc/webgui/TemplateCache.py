bf260245870449afb76597b4e7220f41a94d6990
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-09-09 09:37:11 +0000

    added
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@4090 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ wc/webgui/TemplateCache.py
@@ -0,0 +1,60 @@
+# -*- coding: iso-8859-1 -*-
+"""A template cache."""
+# Copyright (C) 2004  Bastian Kleineidam
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+import os
+import stat
+import errno
+
+import wc.webgui.WebCleanerTemplate
+
+
+class TemplateCache (dict):
+    """Handle requests for templates. Compiled templates are cached
+       for performance reasons and only reloaded if the template file
+       changes.
+    """
+
+    def __getitem__ (self, key):
+        """The key must be a path suitable for an open() and os.stat()
+           call, except when the path is already cached and the file
+           has been deleted. In this case, the already cached template
+           is returned, ignoring the file deletion.
+
+           @return compiled TAL template from given path
+        """
+        if path in self:
+            template = super(TemplateCache, self).__getitem__(key)
+            try:
+                mtime = os.stat(path)[stat.ST_MTIME]
+                if mtime > template.mtime:
+                    # refresh entry
+                    template = wc.webgui.WebCleanerTemplate(path, mtime)
+                    self[path] = template
+            except os.error, msg:
+                # ignore missing files if already cached
+                if msg.errno != errno.ENOENT:
+                    raise
+        else:
+            # new entry
+            mtime = os.stat(path)[stat.ST_MTIME]
+            template = wc.webgui.WebCleanerTemplate(path, mtime)
+            self[path] = template
+        return template
+
+
+templates = TemplateCache()
---
+++ wc/webgui/WebCleanerTemplate.py
@@ -0,0 +1,52 @@
+# -*- coding: iso-8859-1 -*-
+"""TAL template class for WebCleaner"""
+# Copyright (C) 2004  Bastian Kleineidam
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+import wc.webgui.PageTemplates.PageTemplate
+import wc.webgui.PageTemplates.Expressions
+import wc.webgui.TAL.TALInterpreter
+
+
+class WebCleanerTemplate (wc.webgui.PageTemplates.PageTemplate.PageTemplate):
+    """asf asdf """
+
+    def __init__ (self, path, mtime):
+        """Store modification time and compile template from given path."""
+        super(WebCleanerTemplate, self).__init__()
+        self.mtime = mtime
+        self.pt_edit(open(path))
+        if self._v_errors:
+            raise wc.webgui.PageTemplates.PageTemplate.PTRuntimeError(
+                                    'Page Template %s has errors.' % self.id)
+
+    def html (self):
+        """Only HTML is supported at the moment.
+           @return True
+        """
+        return True
+
+    def render (self, context):
+        """Render this Page Template"""
+        __traceback_supplement__ = \
+  (wc.webgui.PageTemplates.PageTemplate.PageTemplateTracebackSupplement, self)
+        output = StringIO.StringIO()
+        engine = wc.webgui.PageTemplates.Expressions.getEngine()
+        wc.webgui.TAL.TALInterpreter.TALInterpreter(
+            self._v_program, self.macros,
+            engine.getContext(context), output, tal=1, strictinsert=0)()
+        return output.getvalue()
+
