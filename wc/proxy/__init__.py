ffc24037e3bca8c47ac2b0491893af175d9e843b
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-09-24 08:59:41 +0000

    use bindaddress also in ssl gateway listener
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@4181 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- wc/proxy/__init__.py
+++ wc/proxy/__init__.py
@@ -132,10 +132,11 @@ def mainloop (handle=None, abort=None):
                                wc.configuration.config['port'],
                                wc.proxy.HttpClient.HttpClient)
     if wc.configuration.config['sslgateway']:
-        wc.proxy.Listener.Listener(wc.configuration.config['sslport'],
-                 wc.proxy.SslClient.SslClient,
-                 sslctx=wc.proxy.ssl.get_serverctx(
-                          wc.configuration.config.configdir))
+        sslctx = wc.proxy.ssl.get_serverctx(wc.configuration.config.configdir)
+        wc.proxy.Listener.Listener(wc.configuration.config['bindaddress'],
+                                   wc.configuration.config['sslport'],
+                                   wc.proxy.SslClient.SslClient,
+                                   sslctx=sslctx)
     # experimental interactive command line
     #from Interpreter import Interpreter
     #Listener(wc.configuration.config['cmdport'], Interpreter)
