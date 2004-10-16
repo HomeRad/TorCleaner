8a3a92b1ed5b5cc43119541619fcf4f23bf0c00e
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-10-16 00:36:11 +0000

    absolute mingw paths
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@4369 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- crossbuild.sh
+++ crossbuild.sh
@@ -6,10 +6,10 @@ HOST=i386-mingw32
 BUILD=i686-pc-linux-gnu
 # for other build boxen you might want to use:
 #BUILD=i386-linux
-CC=i586-mingw32msvc-gcc \
-  CXX=i586-mingw32msvc-g++ \
-  AR=i586-mingw32msvc-ar \
-  RANLIB=i586-mingw32msvc-ranlib \
+CC=/usr/bin/i586-mingw32msvc-gcc \
+  CXX=/usr/bin/i586-mingw32msvc-g++ \
+  AR=/usr/bin/i586-mingw32msvc-ar \
+  RANLIB=/usr/bin/i586-mingw32msvc-ranlib \
  ./configure --target=$TARGET --host=$HOST --build=$BUILD \
  2>&1 | tee crossbuild.log
 make crossbuild 2>&1 | tee --append crossbuild.log
