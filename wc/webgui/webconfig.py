01cc7a6b5cd590e1401cf162881249a18834261f
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-10-18 19:05:30 +0000

    remove unused imports
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@4415 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- wc/webgui/webconfig.py
+++ wc/webgui/webconfig.py
@@ -16,10 +16,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-import os
-import re
-import urllib
-import urlparse
 import gettext
 import mimetypes
 
