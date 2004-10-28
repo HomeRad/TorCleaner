4a23be976d9b50896ccaccc02ae89f5ca6c9f960
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-10-28 07:06:07 +0000

    fixed tests
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@4492 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- wc/tests/test_strformat.py
+++ wc/tests/test_strformat.py
@@ -27,48 +27,48 @@ class TestStrFormat (unittest.TestCase):
 
     def test_unquote (self):
         """test quote stripping"""
-        self.assertEquals(linkcheck.strformat.unquote(""), "")
-        self.assertEquals(linkcheck.strformat.unquote(None), None)
-        self.assertEquals(linkcheck.strformat.unquote("'"), "'")
-        self.assertEquals(linkcheck.strformat.unquote("\""), "\"")
-        self.assertEquals(linkcheck.strformat.unquote("\"\""), "")
-        self.assertEquals(linkcheck.strformat.unquote("''"), "")
-        self.assertEquals(linkcheck.strformat.unquote("'a'"), "a")
-        self.assertEquals(linkcheck.strformat.unquote("'a\"'"), "a\"")
-        self.assertEquals(linkcheck.strformat.unquote("'\"a'"), "\"a")
-        self.assertEquals(linkcheck.strformat.unquote('"a\'"'), 'a\'')
-        self.assertEquals(linkcheck.strformat.unquote('"\'a"'), '\'a')
+        self.assertEquals(wc.strformat.unquote(""), "")
+        self.assertEquals(wc.strformat.unquote(None), None)
+        self.assertEquals(wc.strformat.unquote("'"), "'")
+        self.assertEquals(wc.strformat.unquote("\""), "\"")
+        self.assertEquals(wc.strformat.unquote("\"\""), "")
+        self.assertEquals(wc.strformat.unquote("''"), "")
+        self.assertEquals(wc.strformat.unquote("'a'"), "a")
+        self.assertEquals(wc.strformat.unquote("'a\"'"), "a\"")
+        self.assertEquals(wc.strformat.unquote("'\"a'"), "\"a")
+        self.assertEquals(wc.strformat.unquote('"a\'"'), 'a\'')
+        self.assertEquals(wc.strformat.unquote('"\'a"'), '\'a')
         # even mis-matching quotes should be removed...
-        self.assertEquals(linkcheck.strformat.unquote("'a\""), "a")
-        self.assertEquals(linkcheck.strformat.unquote("\"a'"), "a")
+        self.assertEquals(wc.strformat.unquote("'a\""), "a")
+        self.assertEquals(wc.strformat.unquote("\"a'"), "a")
 
     def test_wrap (self):
         """test line wrapping"""
         s = "11%(sep)s22%(sep)s33%(sep)s44%(sep)s55" % {'sep': os.linesep}
         # testing width <= 0
-        self.assertEquals(linkcheck.strformat.wrap(s, -1), s)
-        self.assertEquals(linkcheck.strformat.wrap(s, 0), s)
+        self.assertEquals(wc.strformat.wrap(s, -1), s)
+        self.assertEquals(wc.strformat.wrap(s, 0), s)
         s2 = "11 22%(sep)s33 44%(sep)s55" % {'sep': os.linesep}
         # splitting lines
-        self.assertEquals(linkcheck.strformat.wrap(s2, 2), s)
+        self.assertEquals(wc.strformat.wrap(s2, 2), s)
         # combining lines
-        self.assertEquals(linkcheck.strformat.wrap(s, 5), s2)
+        self.assertEquals(wc.strformat.wrap(s, 5), s2)
 
     def test_remove_markup (self):
         """test markup removing"""
-        self.assertEquals(linkcheck.strformat.remove_markup("<a>"), "")
-        self.assertEquals(linkcheck.strformat.remove_markup("<>"), "")
-        self.assertEquals(linkcheck.strformat.remove_markup("<<>"), "")
-        self.assertEquals(linkcheck.strformat.remove_markup("a < b"), "a < b")
+        self.assertEquals(wc.strformat.remove_markup("<a>"), "")
+        self.assertEquals(wc.strformat.remove_markup("<>"), "")
+        self.assertEquals(wc.strformat.remove_markup("<<>"), "")
+        self.assertEquals(wc.strformat.remove_markup("a < b"), "a < b")
 
     def test_strsize (self):
         """test byte size strings"""
-        self.assertRaises(ValueError, linkcheck.strformat.strsize, -1)
-        self.assertEquals(linkcheck.strformat.strsize(0), "0 Bytes")
-        self.assertEquals(linkcheck.strformat.strsize(1), "1 Byte")
-        self.assertEquals(linkcheck.strformat.strsize(2), "2 Bytes")
-        self.assertEquals(linkcheck.strformat.strsize(1023), "1023 Bytes")
-        self.assertEquals(linkcheck.strformat.strsize(1024), "1.00 kB")
+        self.assertRaises(ValueError, wc.strformat.strsize, -1)
+        self.assertEquals(wc.strformat.strsize(0), "0 Bytes")
+        self.assertEquals(wc.strformat.strsize(1), "1 Byte")
+        self.assertEquals(wc.strformat.strsize(2), "2 Bytes")
+        self.assertEquals(wc.strformat.strsize(1023), "1023 Bytes")
+        self.assertEquals(wc.strformat.strsize(1024), "1.00 kB")
 
 
 def test_suite ():
