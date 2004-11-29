8edc486660988b6a538c00ffd5383859f5bde535
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-11-29 16:32:57 +0000

    added pretty print option -p
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@4545 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/parsefile.py
+++ test/parsefile.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python2.3
 # -*- coding: iso-8859-1 -*-
 
-def _main ():
+def _main (args):
     """USAGE: test/run.sh test/parsefile.py test.html"""
-    import sys
-    if len(sys.argv)!=2:
+    if len(args) < 1:
         print _main.__doc__
         sys.exit(1)
-    if sys.argv[1] == '-':
+    from wc.HtmlParser.htmllib import HtmlPrinter, HtmlPrettyPrinter
+    if args[0] == "-p":
+        klass = HtmlPrettyPrinter
+        filename = args[1]
+    else:
+        klass = HtmlPrinter
+        filename = args[0]
+    if filename == '-':
         f = sys.stdin
     else:
-        f = file(sys.argv[1])
-    from wc.HtmlParser.htmllib import HtmlPrinter
+        f = open(filename)
     from wc.HtmlParser import htmlsax
-    p = htmlsax.parser(HtmlPrinter())
+    p = htmlsax.parser(klass())
     #p.debug(1)
     size = 1024
     #size = 1
@@ -24,4 +29,5 @@ def _main ():
     p.flush()
 
 if __name__=='__main__':
-    _main()
+    import sys
+    _main(sys.argv[1:])
