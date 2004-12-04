a36b8ef98326c40e38e0239b7877c875072babfd
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-12-04 19:05:56 +0000

    disable binary char filter for unicode
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@4574 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- wc/filter/BinaryCharFilter.py
+++ wc/filter/BinaryCharFilter.py
@@ -17,13 +17,16 @@
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 import string
+import re
 import wc.filter
 import wc.filter.Filter
 
+# for utf charset check (see below)
+is_utf = re.compile(r"text/html;\s*charset=utf-8", re.I).search
 
 class BinaryCharFilter (wc.filter.Filter.Filter):
-    """replace binary characters, often found in Microsoft HTML documents,
-        with their correct HTML equivalent
+    """Replace binary characters, often found in Microsoft HTML documents,
+       with their correct HTML equivalent.
     """
     # which filter stages this filter applies to (see filter/__init__.py)
     orders = [wc.filter.FILTER_RESPONSE_MODIFY]
@@ -35,5 +38,11 @@ class BinaryCharFilter (wc.filter.Filter.Filter):
 
     def doit (self, data, attrs):
         """filter given data"""
+        # the HTML parser does not yet understand Unicode, so this hack
+        # disabled the binary char filter in this case
+        if not attrs.get('binarychar_is_utf'):
+            attrs['binarychar_is_utf'] = is_utf(data)
+        if attrs['binarychar_is_utf']:
+            return data
         return data.translate(string.maketrans('\x00\x84\x91\x92\x93\x94',
                                                ' "`\'""'))
