96f5094f58a26a46449896fc9933ea09c7e03767
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2004-12-27 14:34:48 +0000

    import fix
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@4712 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- wc/webgui/context/restart_html.py
+++ wc/webgui/context/restart_html.py
@@ -16,6 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
+from wc import AppName, Version
 
 # form execution
 def _exec_form (form, lang):
