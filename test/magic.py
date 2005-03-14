491de895422c0a7b81b727cca040c0b5a1bbeba3
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2005-03-14 18:05:34 +0000

    added
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@4949 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ test/magic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.3
+# -*- coding: iso-8859-1 -*-
+
+import sys
+
+def _main (args):
+    import wc.configuration
+    wc.configuration.config = wc.configuration.init()
+    import wc.magic
+    print repr(wc.magic.classify(open(args[0])))
+
+
+if __name__ == '__main__':
+    _main(sys.argv[1:])
