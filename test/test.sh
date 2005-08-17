dd0322fe04953fa35e2aa79770240590eda08b36
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2005-08-17 11:04:35 +0000

    test for resources
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@5256 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/test.sh
+++ test/test.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-test/run.sh test.py --resource=network --search-in=wc -upv "$@"
+if ifconfig eth1 | grep RUNNING > /dev/null; then
+    echo "Network resource available"
+    RES_NETWORK="--resource=network"
+else
+    echo "Network disabled"
+    RES_NETWORK=""
+fi
+if waitfor -w 1 port:localhost:8081; then
+    echo "Proxy resource available"
+    RES_PROXY="--resource=proxy"
+else
+    echo "Proxy disabled"
+    RES_PROXY=""
+fi
+test/run.sh test.py $RES_NETWORK $RES_PROXY --search-in=wc -ufpv "$@"
