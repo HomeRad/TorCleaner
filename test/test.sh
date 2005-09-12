ef2b40a1e56a6d4196eef1ad1e5de331f53b2674
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2005-09-12 23:06:34 +0000

    validate HTML files
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@5437 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 if ifconfig eth1 | grep RUNNING > /dev/null; then
     echo "Network resource available"
     RES_NETWORK="--resource=network"
@@ -6,11 +6,26 @@ else
     echo "Network disabled"
     RES_NETWORK=""
 fi
-if waitfor -w 1 port:localhost:8081; then
+PROXY=localhost:8081
+if waitfor -w 1 port:$PROXY; then
     echo "Proxy resource available"
     RES_PROXY="--resource=proxy"
 else
     echo "Proxy disabled"
     RES_PROXY=""
+fi
+if [ -n "$RES_PROXY" ]; then
+    NAMES="index config filterconfig update rating help"
+    for NAME in $NAMES; do
+        URL="http://$PROXY/${NAME}.html"
+        echo "Validating $URL"
+        curl -s $URL | xmllint --html --noout --valid -
+        URL1="${URL}.de"
+        echo "Validating $URL1"
+        curl -s $URL1 | xmllint --html --noout --valid -
+        URL1="${URL}.en"
+        echo "Validating $URL1"
+        curl -s $URL1 | xmllint --html --noout --valid -
+    done
 fi
 test/run.sh test.py $RES_NETWORK $RES_PROXY --search-in=wc -ufpv "$@"
