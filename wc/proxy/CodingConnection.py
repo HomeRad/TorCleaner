014e348bf69a74a157a5532bf104547517c0cfa9
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2005-09-23 21:03:39 +0000

    added
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@5482 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ wc/proxy/CodingConnection.py
@@ -0,0 +1,67 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2005  Bastian Kleineidam
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+"""
+Handle encoding and decoding.
+"""
+
+import cStringIO as StringIO
+import wc
+import wc.log
+import wc.proxy.StatefulConnection
+from wc.decorators import notimplemented
+
+
+class CodingConnection (wc.proxy.StatefulConnection.StatefulConnection):
+
+    def reset (self):
+        super(CodingConnection, self).reset()
+        # Handle each of these, left to right
+        self.decoders = []
+        self.encoders = []
+        self.chunktrailer = StringIO.StringIO()
+
+    def flush_coders (self, coders, data=""):
+        """
+        Flush given de- or encoders.
+
+        @param data: initial data to process (default: empty string)
+        @ptype data: string
+        @return: flushed data
+        @rtype: string
+        """
+        while coders:
+            wc.log.debug(wc.LOG_PROXY, "flush %s", coders[0])
+            data = coders[0].process(data)
+            data += coders[0].flush()
+            del coders[0]
+        return data
+
+    @notimplemented
+    def filter_headers (self, headers):
+        pass
+
+    def write_trailer (self, data):
+        self.chunktrailer.write(data)
+
+    def handle_trailer (self):
+        self.chunktrailer.seek(0)
+        headers = wc.http.header.WcMessage(self.chunktrailer)
+        # filter headers
+        headers = self.filter_headers(headers)
+        self.chunktrailer.close()
+        return headers
+
