257f9463b84bc87d57c2155baeab863dcd0e5e9f
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2005-10-13 21:06:12 +0000

    quote arguments
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@5659 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # run python interpreter with current dir as search path, and remove all
 # locale settings
-env -u LANGUAGE -u LC_ALL -u LC_CTYPE LANG=C WC_DEVELOPMENT=1 PYTHONPATH=`pwd` python2.4 $*
+env -u LANGUAGE -u LC_ALL -u LC_CTYPE LANG=C WC_DEVELOPMENT=1 PYTHONPATH=`pwd` python2.4 "$@"
