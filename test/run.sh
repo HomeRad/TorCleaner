cd8ccc0e71edffc409de2a792aa9039905271bbc
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2005-10-17 15:37:18 +0000

    store python version in PYVER
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@5675 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # run python interpreter with current dir as search path, and remove all
 # locale settings
-env -u LANGUAGE -u LC_ALL -u LC_CTYPE LANG=C WC_DEVELOPMENT=1 PYTHONPATH=`pwd` python2.4 "$@"
+PYVER=2.4
+env -u LANGUAGE -u LC_ALL -u LC_CTYPE LANG=C WC_DEVELOPMENT=1 PYTHONPATH=`pwd` python$PYVER "$@"
