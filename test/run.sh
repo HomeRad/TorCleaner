748ee3484f26af8601d2c3626abccebaa16abf01
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2005-11-15 22:26:18 +0000

    optimize option
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@5838 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- test/run.sh
+++ test/run.sh
@@ -2,4 +2,5 @@
 # run python interpreter with current dir as search path, and remove all
 # locale settings
 PYVER=2.4
-env -u LANGUAGE -u LC_ALL -u LC_CTYPE LANG=C WC_DEVELOPMENT=1 PYTHONPATH=`pwd` python$PYVER "$@"
+PYOPT=-O
+env -u LANGUAGE -u LC_ALL -u LC_CTYPE LANG=C WC_DEVELOPMENT=1 PYTHONPATH=`pwd` python$PYVER $PYOPT "$@"
