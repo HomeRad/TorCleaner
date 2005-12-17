626223368859184ad72682d9a4d974438b9a5712
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2005-12-17 18:23:32 +0000

    resources
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@5894 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ scripts/resources.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+# network device, change as appropriate
+NETDEV=ppp0
+if ifconfig $NETDEV | grep RUNNING > /dev/null; then
+    echo "--resource=network"
+fi
+
+if msgfmt -V > /dev/null 2>&1; then
+    echo "--resource=msgfmt"
+fi
+
+PROXY=localhost:8081
+if waitfor -w 1 port:$PROXY; then
+    echo "--resource=proxy"
+fi
