303e91e12ee2be5845b541e2c22fbcaf67136149
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2006-01-05 14:37:43 +0000

    precompute character map
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@6014 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- wc/filter/BinaryCharFilter.py
+++ wc/filter/BinaryCharFilter.py
@@ -26,20 +26,19 @@ import wc.filter.Filter
 # for utf charset check (see below)
 is_utf = re.compile(r"text/html;\s*charset=utf-8", re.I).search
 
-def get_character_map ():
-    """
-    Construct character replacement table.
-    """
-    return (
-        # Replace Null byte with space
-        (chr(0x0), chr(0x20)),  # ' '
-        # Replace Microsoft quotes
-        (chr(0x84), chr(0x22)), # '"'
-        (chr(0x91), chr(0x60)), # '`'
-        (chr(0x92), chr(0x27)), # '\''
-        (chr(0x93), chr(0x22)), # '"'
-        (chr(0x94), chr(0x22)), # '"'
-    )
+# character replacement table.
+charmap = (
+    # Replace Null byte with space
+    (chr(0x0), chr(0x20)),  # ' '
+    # Replace Microsoft quotes
+    (chr(0x84), chr(0x22)), # '"'
+    (chr(0x91), chr(0x60)), # '`'
+    (chr(0x92), chr(0x27)), # '\''
+    (chr(0x93), chr(0x22)), # '"'
+    (chr(0x94), chr(0x22)), # '"'
+)
+charmap_in = ''.join(x[0] for x in charmap)
+charmap_out = ''.join(x[1] for x in charmap)
 
 
 class BinaryCharFilter (wc.filter.Filter.Filter):
@@ -57,10 +56,7 @@ class BinaryCharFilter (wc.filter.Filter.Filter):
         stages = [wc.filter.STAGE_RESPONSE_MODIFY]
         mimes = ['text/html']
         super(BinaryCharFilter, self).__init__(stages=stages, mimes=mimes)
-        charmapping = get_character_map()
-        _in = ''.join(x[0] for x in charmapping)
-        _out = ''.join(x[1] for x in charmapping)
-        self.transe = string.maketrans(_in, _out)
+        self.transe = string.maketrans(charmap_in, charmap_out)
 
     def doit (self, data, attrs):
         """
