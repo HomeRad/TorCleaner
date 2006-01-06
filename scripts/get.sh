124370fe22115f9893c7b177ae1d57f63dcbbb11
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2006-01-06 21:48:17 +0000

    use wget
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@6039 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-curl -i -k -x localhost:8081 "$1"
+env http_proxy="http://localhost:8081" wget --no-check-certificate -O /dev/null "$1"
+# note: curl has problems with chunked encoding
+#TRACE=--trace curltrace.txt
+#curl $TRACE -i -k -x localhost:8081 "$1"
