b6ec532e7e73c36fa130ac790af0cd040d94c6a8
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2006-01-12 00:22:34 +0000

    bump up version
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@6069 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- setup.py
+++ setup.py
@@ -620,7 +620,7 @@ if os.name == 'posix':
 myname = "Bastian Kleineidam"
 myemail = "calvin@users.sourceforge.net"
 setup (name = "webcleaner",
-       version = "2.36",
+       version = "2.37",
        description = "a filtering HTTP proxy",
        keywords = "proxy,server,http,filters,daemon",
        author = myname,
