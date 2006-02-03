a8823ac3ace1b734c00ffb895851ef0ed5b4c1d4
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2006-02-03 17:14:56 +0000

    cleanup
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@6139 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # run python interpreter with current dir as search path, and remove all
-# locale settings
+# locale and proxy settings
 PYVER=2.4
-#PYOPT=-O
-env -u LANGUAGE -u LC_ALL -u LC_CTYPE LANG=C WC_DEVELOPMENT=1 PYTHONPATH=`pwd` python$PYVER $PYOPT "$@"
+PYOPTS=
+#PYOPTS=-O
+env -u ftp_proxy -u http_proxy -u LANGUAGE -u LC_ALL -u LC_CTYPE LANG=C PYTHONPATH=`pwd` python${PYVER} $PYOPTS "$@"
