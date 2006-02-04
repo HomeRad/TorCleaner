80507b2126396a5815d77b6f1b04b804a0eb0a03
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2006-02-04 16:38:32 +0000

    default is file output
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@6162 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-env http_proxy="http://localhost:8081" wget --no-check-certificate -O /dev/null "$1"
+#NOOUT=-O /dev/null
+env http_proxy="http://localhost:8081" wget --no-check-certificate ${NOOUT:-} "$1"
 # note: curl has problems with chunked encoding
 #TRACE=--trace curltrace.txt
 #curl $TRACE -i -k -x localhost:8081 "$1"
