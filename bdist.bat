5d76e144e606883b4fd01a5cd5cdf318726b94cd
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2006-03-31 16:43:14 +0000

    use newer xn resource editor, and adjust Python path
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@6303 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- bdist.bat
+++ bdist.bat
@@ -1,7 +1,7 @@
 @echo off
 REM batch file for generating the windows .exe installer
-set PYTHON="c:\Python24\python.exe"
+set PYTHON="c:\Python 24\python.exe"
 %PYTHON% setup.py sdist --manifest-only
 %PYTHON% setup.py build -c mingw32 bdist_wininst -b tacho.bmp
 REM start resource editor for .exe icon
-"%PROGRAMFILES%\PE Resource Explorer\PEResourceExplorer.exe"
+"%PROGRAMFILES%\XN Resource Editor\XNResourceEditor.exe"
