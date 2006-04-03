18420d70a1bc31a61f0befcc0f561957062a7b79
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2006-04-03 22:41:08 +0000

    clean before building
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@6323 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- bdist.bat
+++ bdist.bat
@@ -1,6 +1,7 @@
 @echo off
 REM batch file for generating the windows .exe installer
 set PYTHON="c:\Python 24\python.exe"
+%PYTHON% setup.py clean --all
 %PYTHON% setup.py sdist --manifest-only
 %PYTHON% setup.py build -c mingw32 bdist_wininst -b tacho.bmp
 REM start resource editor for .exe icon
