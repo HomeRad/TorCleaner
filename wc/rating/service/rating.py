0fd84d2ac0442a1bb6c6137c33a3150465801ba9
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2006-05-05 20:27:45 +0000

    added
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@6484 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

---
+++ wc/rating/service/rating.py
@@ -0,0 +1,36 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2006 Bastian Kleineidam
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+"""
+Rating import and export to various formats.
+"""
+
+import wc.rating
+
+
+def rating_from_headers (headers):
+    """
+    Parse X-Rating-* HTTP headers into a rating dictionary.
+    @param headers: server headers
+    @ptype headers: wc.http.header.WcMessage
+    @return: parsed rating
+    @rtype: wc.rating.Rating
+    """
+    rating = wc.rating.Rating()
+    for name, value in headers:
+        if name.lower().startswith("x-rating-"):
+            rating[name[9:]] = value
+    return rating
