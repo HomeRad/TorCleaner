2a1fc63f7785226a7551af8c112a9cd078f26c6f
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2006-10-16 11:37:29 +0000

    python 2.5 usage
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@6700 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- scripts/build.bat
+++ scripts/build.bat
@@ -2,6 +2,6 @@
 set PYTHON="c:\Python 25\python.exe"
 %PYTHON% setup.py sdist --manifest-only
 %PYTHON% setup.py build -c mingw32
-copy build\lib.win32-2.4\wc\HtmlParser\htmlsax.pyd wc\HtmlParser
-copy build\lib.win32-2.4\wc\js\jslib.pyd wc\js
-copy build\lib.win32-2.4\wc\levenshtein.pyd wc
+copy build\lib.win32-2.5\wc\HtmlParser\htmlsax.pyd wc\HtmlParser
+copy build\lib.win32-2.5\wc\js\jslib.pyd wc\js
+copy build\lib.win32-2.5\wc\levenshtein.pyd wc
