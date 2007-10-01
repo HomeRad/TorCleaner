1bc53d9d4c6532efa8ae86e0e01f33f27705bf3c
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2007-10-01 20:51:16 +0000

    use autoreconf
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@6782 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- autogen.sh
+++ autogen.sh
@@ -1,37 +1,15 @@
 #!/bin/sh
 # Generate the Makefiles and configure files
-if !( aclocal --version ) </dev/null > /dev/null 2>&1; then
-    echo "aclocal not found -- aborting"
+if !( autoreconf --version ) </dev/null > /dev/null 2>&1; then
+    echo "autoreconf not found -- aborting"
     exit
 fi
 
-if !( autoheader --version ) </dev/null > /dev/null 2>&1; then
-    echo "autoheader not found -- aborting"
-    exit
-fi
-
-if !( automake --version ) </dev/null > /dev/null 2>&1; then
-    echo "automake not found -- aborting"
-    exit
-fi
-
-if !( libtoolize --version ) </dev/null > /dev/null 2>&1; then
-    echo "libtoolize not found -- aborting"
-    exit
-fi
-
-if !( autoconf --version ) </dev/null > /dev/null 2>&1; then
-    echo "autoconf not found -- aborting"
-    exit
-fi
-echo "Building ltmain" && libtoolize --copy --force --automake && \
-echo "Building macros" && aclocal && \
-echo "Building config header template" && autoheader && \
-echo "Building Makefiles" && automake --add-missing --gnu --copy && \
-echo "Building configure" && autoconf
+echo "Updating generated configuration files with autoreconf..." && autoreconf --force --install --verbose
 RES=$?
 if [ $RES != 0 ]; then
     echo "Autogeneration failed (exit code $RES)"
     exit $RES
 fi
+rm -rf autom4te*.cache
 echo 'run "./configure && make localbuild"'
