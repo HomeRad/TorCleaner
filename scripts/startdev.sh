df91e16d8c76814a1b77f9cf96dc20e0370f1335
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2007-12-03 13:12:09 +0000

    directly call python
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@6808 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- scripts/startdev.sh
+++ scripts/startdev.sh
@@ -3,4 +3,7 @@ CONFIG=${1:-localconfig}
 if [ "$CONFIG" = "localconfig" ]; then
     export WC_DEVELOPMENT=1
 fi
-`dirname $0`/run.sh webcleaner --config=$CONFIG --no-file-logs
+PYVER=2.4
+exec env -u ftp_proxy -u http_proxy -u LANGUAGE -u LC_ALL -u LC_CTYPE \
+  LANG=C PYTHONPATH=`pwd` python${PYVER} ${PYOPTS:-} \
+  webcleaner --config=$CONFIG --no-file-logs
