d61f382c1a34af06b9dbff0bf6783952001c2875
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2008-05-20 17:28:54 +0000

    Don't use resource editor anymore
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@6862 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- scripts/bdist.bat
+++ scripts/bdist.bat
@@ -5,4 +5,4 @@ set PYTHON="c:\Python 25\python.exe"
 %PYTHON% setup.py sdist --manifest-only
 %PYTHON% setup.py build -c mingw32 bdist_wininst -b tacho.bmp
 REM start resource editor for .exe icon
-"%PROGRAMFILES%\XN Resource Editor\XNResourceEditor.exe"
+REM "%PROGRAMFILES%\XN Resource Editor\XNResourceEditor.exe"
