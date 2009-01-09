798ed584c74fd33a10a9cef1c1bd9d3cde6d131b
calvin <calvin@e058641e-7c0d-0410-ba67-d7ca6cf85b4a>
2009-01-09 09:10:17 +0000

    Updated tests
    git-svn-id: https://webcleaner.svn.sourceforge.net/svnroot/webcleaner/trunk/webcleaner2@6887 e058641e-7c0d-0410-ba67-d7ca6cf85b4a

--- tests/test_ip.py
+++ tests/test_ip.py
@@ -1,99 +1,99 @@
 # -*- coding: iso-8859-1 -*-
 
 import unittest
-import wc.ip
+from wc.network import iputil
 
 
 class TestIp (unittest.TestCase):
 
     def testNames (self):
-        hosts, nets = wc.ip.hosts2map(["www.kampfesser.net",
+        hosts, nets = iputil.hosts2map(["www.kampfesser.net",
                                     "q2345qwer9 u2 42ß3 i34 uq3tu ",
                                     "2.3.4", ".3.4", "3.4", ".4", "4", ""])
-        for host in wc.ip.resolve_host("www.kampfesser.net"):
-            self.assert_(wc.ip.host_in_set(host, hosts, nets))
-        self.assert_(not wc.ip.host_in_set("q2345qwer9 u2 42ß3 i34 uq3tu ", hosts, nets))
-        self.assert_(not wc.ip.host_in_set("q2345qwer9", hosts, nets))
-        self.assert_(not wc.ip.host_in_set("2.3.4", hosts, nets))
-        self.assert_(not wc.ip.host_in_set(".3.4", hosts, nets))
-        self.assert_(not wc.ip.host_in_set("3.4", hosts, nets))
-        self.assert_(not wc.ip.host_in_set(".4", hosts, nets))
-        self.assert_(not wc.ip.host_in_set("4", hosts, nets))
-        self.assert_(not wc.ip.host_in_set("", hosts, nets))
+        for host in iputil.resolve_host("www.kampfesser.net"):
+            self.assert_(iputil.host_in_set(host, hosts, nets))
+        self.assert_(not iputil.host_in_set("q2345qwer9 u2 42ß3 i34 uq3tu ", hosts, nets))
+        self.assert_(not iputil.host_in_set("q2345qwer9", hosts, nets))
+        self.assert_(not iputil.host_in_set("2.3.4", hosts, nets))
+        self.assert_(not iputil.host_in_set(".3.4", hosts, nets))
+        self.assert_(not iputil.host_in_set("3.4", hosts, nets))
+        self.assert_(not iputil.host_in_set(".4", hosts, nets))
+        self.assert_(not iputil.host_in_set("4", hosts, nets))
+        self.assert_(not iputil.host_in_set("", hosts, nets))
 
     def testIPv4_1 (self):
-        hosts, nets = wc.ip.hosts2map(["1.2.3.4"])
-        self.assert_(wc.ip.host_in_set("1.2.3.4", hosts, nets))
+        hosts, nets = iputil.hosts2map(["1.2.3.4"])
+        self.assert_(iputil.host_in_set("1.2.3.4", hosts, nets))
 
     def testBitmask0 (self):
         """test the catch-all ip bitmask"""
-        hosts, nets = wc.ip.hosts2map(["192.168.1.1/0"])
-        self.assert_(wc.ip.host_in_set("1.1.1.1", hosts, nets))
-        self.assert_(wc.ip.host_in_set("0.0.0.0", hosts, nets))
-        self.assert_(wc.ip.host_in_set("77.88.33.1", hosts, nets))
-        self.assert_(wc.ip.host_in_set("255.255.255.255", hosts, nets))
+        hosts, nets = iputil.hosts2map(["192.168.1.1/0"])
+        self.assert_(iputil.host_in_set("1.1.1.1", hosts, nets))
+        self.assert_(iputil.host_in_set("0.0.0.0", hosts, nets))
+        self.assert_(iputil.host_in_set("77.88.33.1", hosts, nets))
+        self.assert_(iputil.host_in_set("255.255.255.255", hosts, nets))
 
     def testBitmask8 (self):
-        hosts, nets = wc.ip.hosts2map(["192.168.1.1/8"])
+        hosts, nets = iputil.hosts2map(["192.168.1.1/8"])
         for i in xrange(255):
-            self.assert_(wc.ip.host_in_set("192.168.1.%d"%i, hosts, nets))
+            self.assert_(iputil.host_in_set("192.168.1.%d"%i, hosts, nets))
 
     def testBitmask16 (self):
-        hosts, nets = wc.ip.hosts2map(["192.168.1.1/16"])
+        hosts, nets = iputil.hosts2map(["192.168.1.1/16"])
         for i in xrange(255):
             for j in xrange(255):
-                self.assert_(wc.ip.host_in_set("192.168.%d.%d"%(i,j), hosts, nets))
+                self.assert_(iputil.host_in_set("192.168.%d.%d"%(i,j), hosts, nets))
 
     def testBitmask32 (self):
-        hosts, nets = wc.ip.hosts2map(["192.168.1.1/32"])
-        self.assert_(not wc.ip.host_in_set("1.1.1.1", hosts, nets))
-        self.assert_(not wc.ip.host_in_set("0.0.0.0", hosts, nets))
-        self.assert_(not wc.ip.host_in_set("77.88.33.1", hosts, nets))
-        self.assert_(wc.ip.host_in_set("255.255.255.255", hosts, nets))
+        hosts, nets = iputil.hosts2map(["192.168.1.1/32"])
+        self.assert_(not iputil.host_in_set("1.1.1.1", hosts, nets))
+        self.assert_(not iputil.host_in_set("0.0.0.0", hosts, nets))
+        self.assert_(not iputil.host_in_set("77.88.33.1", hosts, nets))
+        self.assert_(iputil.host_in_set("255.255.255.255", hosts, nets))
 
     def testNetmask1 (self):
-        hosts, nets = wc.ip.hosts2map(["192.168.1.1/255.255.255.0"])
+        hosts, nets = iputil.hosts2map(["192.168.1.1/255.255.255.0"])
         for i in xrange(255):
-            self.assert_(wc.ip.host_in_set("192.168.1.%d"%i, hosts, nets))
+            self.assert_(iputil.host_in_set("192.168.1.%d"%i, hosts, nets))
 
     def testNetmask2 (self):
-        hosts, nets = wc.ip.hosts2map(["192.168.1.1/255.255.0.0"])
+        hosts, nets = iputil.hosts2map(["192.168.1.1/255.255.0.0"])
         for i in xrange(255):
             for j in xrange(255):
-                self.assert_(wc.ip.host_in_set("192.168.%d.%d"%(i,j), hosts, nets))
+                self.assert_(iputil.host_in_set("192.168.%d.%d"%(i,j), hosts, nets))
 
     def testNetmask4 (self):
         """test the catch-all netmask"""
-        hosts, nets = wc.ip.hosts2map(["192.168.1.1/0.0.0.0"])
-        self.assert_(wc.ip.host_in_set("1.1.1.1", hosts, nets))
-        self.assert_(wc.ip.host_in_set("0.0.0.0", hosts, nets))
-        self.assert_(wc.ip.host_in_set("77.88.33.1", hosts, nets))
-        self.assert_(wc.ip.host_in_set("255.255.255.255", hosts, nets))
-        hosts, nets = wc.ip.hosts2map(["1.1.1.1/0.0.0.0"])
-        self.assert_(wc.ip.host_in_set("1.1.1.1", hosts, nets))
-        self.assert_(wc.ip.host_in_set("0.0.0.0", hosts, nets))
-        self.assert_(wc.ip.host_in_set("77.88.33.1", hosts, nets))
-        self.assert_(wc.ip.host_in_set("255.255.255.255", hosts, nets))
+        hosts, nets = iputil.hosts2map(["192.168.1.1/0.0.0.0"])
+        self.assert_(iputil.host_in_set("1.1.1.1", hosts, nets))
+        self.assert_(iputil.host_in_set("0.0.0.0", hosts, nets))
+        self.assert_(iputil.host_in_set("77.88.33.1", hosts, nets))
+        self.assert_(iputil.host_in_set("255.255.255.255", hosts, nets))
+        hosts, nets = iputil.hosts2map(["1.1.1.1/0.0.0.0"])
+        self.assert_(iputil.host_in_set("1.1.1.1", hosts, nets))
+        self.assert_(iputil.host_in_set("0.0.0.0", hosts, nets))
+        self.assert_(iputil.host_in_set("77.88.33.1", hosts, nets))
+        self.assert_(iputil.host_in_set("255.255.255.255", hosts, nets))
 
     def testIPv6_1 (self):
-        hosts, nets = wc.ip.hosts2map(["::0"])
+        hosts, nets = iputil.hosts2map(["::0"])
         # XXX
 
     def testIPv6_2 (self):
-        hosts, nets = wc.ip.hosts2map(["1::"])
+        hosts, nets = iputil.hosts2map(["1::"])
         # XXX
 
     def testIPv6_3 (self):
-        hosts, nets = wc.ip.hosts2map(["1::1"])
+        hosts, nets = iputil.hosts2map(["1::1"])
         # XXX
 
     def testIPv6_4 (self):
-        hosts, nets = wc.ip.hosts2map(["fe00::0"])
+        hosts, nets = iputil.hosts2map(["fe00::0"])
         # XXX
 
     def testNetmask (self):
         for i in xrange(32):
-            hosts, nets = wc.ip.hosts2map(["144.145.146.1/%d"%i])
+            hosts, nets = iputil.hosts2map(["144.145.146.1/%d"%i])
 
 
 def test_suite ():
