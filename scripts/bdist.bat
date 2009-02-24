d41f086ac6ae97fe7a867ea320fdb3bc6441d7cb
Bastian Kleineidam <calvin@debian.org>
2009-02-24 18:03:55 +0100

    Remove tacho.bmp

--- scripts/bdist.bat
+++ scripts/bdist.bat
@@ -3,6 +3,6 @@ REM batch file for generating the windows .exe installer
 set PYTHON="c:\Python25\python.exe"
 %PYTHON% setup.py clean --all
 %PYTHON% setup.py sdist --manifest-only
-%PYTHON% setup.py build -c mingw32 bdist_wininst -b tacho.bmp
+%PYTHON% setup.py build -c mingw32 bdist_wininst
 REM start resource editor for .exe icon
 REM "%PROGRAMFILES%\XN Resource Editor\XNResourceEditor.exe"
