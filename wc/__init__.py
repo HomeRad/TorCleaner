2978eb4b0d57101bb171bfb1b774a08303d839b7
Bastian Kleineidam <calvin@debian.org>
2009-02-28 12:51:59 +0100

    Use Python 2.6

--- wc/__init__.py
+++ wc/__init__.py
@@ -20,8 +20,8 @@ Basic start and init methods.
 
 import sys
 if not (hasattr(sys, 'version_info') or
-        sys.version_info < (2, 5, 0, 'final', 0)):
-    raise SystemExit("This program requires Python 2.5 or later.")
+        sys.version_info < (2, 6, 0, 'final', 0)):
+    raise SystemExit("This program requires Python 2.6 or later.")
 import os
 import ConfigParser
 import logging.config
